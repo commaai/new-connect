dec7a33fcb7f0388fd9940d590830559f19e42f2
Cameron Clough <cameronjclough@gmail.com>
2024-06-13 21:19:04 +0100

    setup vitest and @solidjs/testing-library (#24)

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { Suspense, lazy, type VoidComponent } from 'solid-js'
+import { Router, Route } from '@solidjs/router'
+
+const Login = lazy(() => import('./pages/auth/login'))
+const Logout = lazy(() => import('./pages/auth/logout'))
+const Auth = lazy(() => import('./pages/auth/auth'))
+
+const Dashboard = lazy(() => import('./pages/dashboard'))
+
+const App: VoidComponent = () => {
+  return (
+    <Router root={(props) => <Suspense>{props.children}</Suspense>}>
+      <Route path="/login" component={Login} />
+      <Route path="/logout" component={Logout} />
+      <Route path="/auth" component={Auth} />
+
+      <Route path="/*dongleId" component={Dashboard} />
+    </Router>
+  )
+}
+
+export default App
