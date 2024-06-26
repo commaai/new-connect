e2f3490348284d2dd770f0edd6dfafbb144da1f3
vishalkrishnads <321vishalds@gmail.com>
2024-06-26 11:34:57 +0530

    implemented filters

--- src/pages/dashboard/components/RouteList.tsx
+++ src/pages/dashboard/components/RouteList.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import {
   createEffect,
-  createResource,
   createSignal,
   For,
   Show,
@@ -12,6 +11,32 @@ import type { RouteSegments } from '~/types'
 import { getKey, getRouteCardsData } from '~/api/routelist'
 import RouteCard from '~/components/RouteCard'
 import Loader from '~/components/Loader'
+import Button from '~/components/material/Button'
+
+type Filter = {
+  label: string
+  sorter?: (a: RouteSegments, b: RouteSegments) => number
+}
+const filters: Filter[] = [
+  {
+    label: 'Miles',
+    sorter: (a: RouteSegments, b: RouteSegments) => { 
+      return (b.ui_derived?.distance || 0) - (a.ui_derived?.distance || 0)
+    },
+  },
+  {
+    label: 'Duration',
+    sorter: (a: RouteSegments, b: RouteSegments) => { 
+      return (b.ui_derived?.duration?.asMilliseconds() || 0) - (a.ui_derived?.duration?.asMilliseconds() || 0)
+    },
+  },
+  {
+    label: 'Engagement',
+    sorter: (a: RouteSegments, b: RouteSegments) => { 
+      return (b.ui_derived?.engagement || 0) - (a.ui_derived?.engagement || 0)
+    },
+  },
+]
 
 const PAGE_SIZE = 5
 const pages: Promise<RouteSegments[]>[] = []
@@ -23,11 +48,14 @@ type Props = {
 
 const RouteList: VoidComponent<Props> = (props) => {
   
-  const [searchResults, setSearchResults] = createSignal<RouteSegments[]>([])
-
   const dongleId = () => props.dongleId
   const query = () => props.searchQuery
-  let cache: RouteSegments[] = []
+  
+  let routes: RouteSegments[] = []
+
+  const [display, setDisplay] = createSignal<RouteSegments[]>([])
+  const [pageSize, setSize] = createSignal<number>(0)
+  const [filter, setFilter] = createSignal<Filter>({ label: '' })
 
   const getPage = (page: number): Promise<RouteSegments[]> => {
     if (!pages[page]) {
@@ -36,48 +64,60 @@ const RouteList: VoidComponent<Props> = (props) => {
         const previousPageData = page > 0 ? await getPage(page - 1) : undefined
         const key = getKey(dongleId(), PAGE_SIZE, previousPageData)
         const data = await getRouteCardsData(key)
-        cache = data
         resolve(data)
       })
     }
     return pages[page]
   }
 
-  createEffect(() => {
-    if (dongleId()) {
-      pages.length = 0
-      setSize(1)
-    }
-  })
-
-  createEffect(() => {
-    if (query()) {
-      const results = cache.filter(route => {
+  const searchResults = (searchQuery: string | undefined) => {
+    let results = routes
+    if(searchQuery) {
+      results = routes.filter(route => {
         const address = route.ui_derived?.address
-        return address?.start.toLowerCase().includes(query()) || address?.end.toLowerCase().includes(query())
+        return address?.start.toLowerCase().includes(searchQuery) || address?.end.toLowerCase().includes(searchQuery)
       })
-      setSearchResults(results)
-    } else {
-      setSearchResults([])
     }
+    return results
+  }
+
+  const filteredResults = () => {
+    let results = routes
+    if(filter().label != '') {
+      results = results.slice().sort(filter().sorter)
+    }
+    return results
+  }
+
+  createEffect(() => {
+    getPage(pageSize())
+      .then(res => {
+        routes = routes.concat(res)
+        routes = searchResults(query())
+        routes = filteredResults()
+        setDisplay(routes)
+      })
+      .catch(() => {})
   })
 
-  const [size, setSize] = createSignal(1)
-  const pageNumbers = () => Array.from(Array(size()).keys())
+  createEffect(() => {
+    setDisplay(searchResults(query()))
+  })
 
   const Filters: VoidComponent = () => {
-    const [selected, setSelected] = createSignal('')
 
-    const filters = ['miles', 'duration', 'engaged']
     return (
       <div class="flex h-40 w-full items-end space-x-4 p-4">
         <For each={filters}>
-          {(filter) => (
+          {(each) => (
             <div
-              onClick={() => setSelected(filter === selected() ? '' : filter)}
-              class={`group flex items-center justify-center rounded-lg border-2 border-secondary-container px-4 py-2 ${selected() === filter ? 'bg-primary-container' : 'hover:bg-secondary-container'}`}
+              onClick={() => {
+                setFilter(each.label === filter().label ? { label: '' } : each)
+                setDisplay(filteredResults())
+              }}
+              class={`group flex items-center justify-center rounded-lg border-2 border-secondary-container px-4 py-2 ${each.label === filter().label ? 'bg-primary-container' : 'hover:bg-secondary-container'}`}
             >
-              <p class={`${selected() === filter ? 'text-on-primary-container' : 'text-on-secondary-container group-hover:text-primary'}`}>{filter}</p>
+              <p class={`${each.label === filter().label ? 'text-on-primary-container' : 'text-on-secondary-container group-hover:text-primary'}`}>{each.label}</p>
             </div>
           )}
         </For>
@@ -86,30 +126,17 @@ const RouteList: VoidComponent<Props> = (props) => {
   }
 
   return (
-    <Show when={searchResults().length <= 0} fallback={
-      <>
-        <Filters />
-        <For each={searchResults()}>
-          {(route) => <RouteCard route={route} />}
-        </For>
-        <div class="h-60 w-full sm:h-44" />
-      </>
-    }>
-      <For each={pageNumbers()}>
-        {(i) => {
-          const [routes] = createResource(() => i, getPage)
-          return (
-            <Suspense fallback={<div class="flex size-full items-center justify-center"><Loader /></div>}>
-              <Filters />
-              <For each={routes()}>
-                {(route) => <RouteCard route={route} />}
-              </For>
-              <div class="h-60 w-full sm:h-44" />
-            </Suspense>
-          )
-        }}
+    <Suspense fallback={<div class="flex size-full items-center justify-center"><Loader /></div>} >
+      <Filters /><For each={display()}>
+        {(route) => <RouteCard route={route} />}
       </For>
-    </Show>
+      <Show when={display().length > 0}>
+        <div class="h-20 w-full flex items-center justify-center">
+          <Button onClick={() => setSize(pageSize() + 1)}>Load more</Button>
+        </div>
+      </Show>
+      <div class="h-60 w-full sm:h-44" />
+    </Suspense>
   )
 }
 
