c20808e92ca8f62e3358d9e925812a6596632966
vishalkrishnads <321vishalds@gmail.com>
2024-07-04 11:28:27 +0530

    reduced file count

--- malformer/__main__.py
+++ malformer/__main__.py
@@ -1,8 +1,16 @@
-from utils.downloader import Downloader
-from utils.args import get_cli_args
 from utils.defaults import CAPNP_MAP
-from corrupter import Corrupter
+from malformer import Corrupter, Downloader
 from bullet import Check
+import argparse
+
+def get_cli_args():
+    parser = argparse.ArgumentParser(description="A script to corrupt openpilot routes for comma connect")
+    parser.add_argument('-a', '--account', type=str, help="JWT access token for comma account")
+    parser.add_argument('-d', '--device', type=str, help="dongleId of the device from which route was uploaded")
+    parser.add_argument('-r', '--route', type=str, help="Unique ID of the route to be corrupted")
+
+    args = parser.parse_args()
+    return args.account, args.device, args.route
 
 def get_params():
     print("\nSelect what all parameters you want corrupted in this route. (use UP & DOWN to scroll, SPACE to select/unselect)\n")
--- malformer/utils/defaults.py
+++
@@ -1,6 +0,0 @@
-ACCOUNT = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDg1ODI0NjUsIm5iZiI6MTcxNzA0NjQ2NSwiaWF0IjoxNzE3MDQ2NDY1LCJpZGVudGl0eSI6IjBkZWNkZGNmZGYyNDFhNjAifQ.g3khyJgOkNvZny6Vh579cuQj1HLLGSDeauZbfZri9jw'
-DEVICE = '1d3dc3e03047b0c7'
-ROUTE = '000000dd--455f14369d'
-FPS = 20 # video frame rate, for performing calculations on qlogs
-STORAGE_PATH = './malformer/routes'
-CAPNP_MAP = { 'GPS': 'gpsLocation', 'Thumbnail': 'thumbnail', 'qlogs': 'qEncodeIdx' }
