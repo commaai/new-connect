e8f7e8707837dcd7373c66cee14e17c40cc394cb
signed-long <38993953+signed-long@users.noreply.github.com>
2024-07-03 18:02:27 -0700

    use chromium not webkit

--- src/ci/screenshots.cjs
+++ src/ci/screenshots.cjs
@@ -1,5 +1,5 @@
 /* eslint-disable */
-const { webkit, devices } = require('playwright');
+const { chromium, devices } = require('playwright');
 
 const base_url = process.argv[2];
 const out_dir = process.argv[3] ? process.argv[3] : 'screenshots';
@@ -23,13 +23,13 @@ async function takeScreenshots(deviceType, context) {
 }
 
 (async () => {
-  const mobile_browser = await webkit.launch()
+  const mobile_browser = await chromium.launch()
   const iphone_13 = devices['iPhone 13']
   const mobile_context = await mobile_browser.newContext(iphone_13)
   await takeScreenshots('mobile', mobile_context)
   await mobile_browser.close()
 
-  const desktop_browser = await webkit.launch()
+  const desktop_browser = await chromium.launch()
   const desktop_context = await desktop_browser.newContext({viewport: { width: 1920, height: 1080 }})
   await takeScreenshots('desktop', desktop_context)
   await desktop_browser.close()
