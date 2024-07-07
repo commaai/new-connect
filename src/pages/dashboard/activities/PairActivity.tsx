fe477846145f8974b3e48f276f54067494ce097a
Cameron Clough <cameronjclough@gmail.com>
2024-07-07 23:26:56 +0100

    improved style/layout

--- src/pages/dashboard/activities/PairActivity.tsx
+++ src/pages/dashboard/activities/PairActivity.tsx
@@ -28,13 +28,13 @@ const PairActivity: VoidComponent = () => {
   })
 
   return (
-    <div id="video-container" class="absolute size-full overflow-hidden bg-black">
+    <div id="video-container" class="absolute size-full overflow-hidden bg-black text-white">
       <video class="left-1/2 top-1/2 h-full min-w-full -translate-x-1/2 -translate-y-1/2 object-cover" ref={videoRef!} />
-      <div class="absolute inset-x-0 top-0 flex h-16 px-4 py-5 text-on-surface">
-        <h1 class="grow text-title-lg">Pair new device</h1>
-        <IconButton href="/">close</IconButton>
-      </div>
-      <div class="prose absolute inset-x-0 bottom-0 px-4 py-5">
+      <div class="prose absolute inset-0 flex flex-col justify-between p-4">
+        <div class="flex items-center">
+          <h1 class="grow text-title-lg">Pair new device</h1>
+          <IconButton href="/">close</IconButton>
+        </div>
         <h2 class="text-center text-title-md">Use the viewfinder to scan the QR code on your device</h2>
       </div>
     </div>
