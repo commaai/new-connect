efd7aa6cc9af5f0a5b903d9615daa7991d34b972
Cameron Clough <cameronjclough@gmail.com>
2024-07-12 00:34:42 +0100

    basic layout

--- src/api/prime.ts
+++ src/api/prime.ts
@@ -20,6 +20,24 @@ export const getSubscriptionStatus = async (dongleId: string) => {
   return fetcher<SubscriptionStatus>(`/v1/prime/subscription?${params.toString()}`, undefined, BILLING_URL)
 }
 
+interface SubscribeInfo {
+  data_connected: boolean | null
+  device_online: boolean
+  has_prime: boolean
+  is_prime_sim: boolean
+  sim_id: string | null
+  sim_type: string | null
+  sim_usable: boolean | null
+  trial_end_data: number | null,
+  trial_end_nodata: number | null,
+}
+
+export const getSubscribeInfo = async (dongleId: string) => {
+  const params = new URLSearchParams()
+  params.append('dongle_id', dongleId)
+  return fetcher<SubscribeInfo>(`/v1/prime/subscribe_info?${params.toString()}`, undefined, BILLING_URL)
+}
+
 interface ActivateSubscriptionRequest {
   dongle_id: string
   sim_id: string | null
--- src/pages/dashboard/activities/PrimeActivity.tsx
+++ src/pages/dashboard/activities/PrimeActivity.tsx
@@ -1,18 +1,55 @@
-import { type VoidComponent } from 'solid-js'
+import { createResource, Match, Show, Switch, type VoidComponent } from 'solid-js'
+import { getDevice } from '~/api/devices'
+import { getSubscribeInfo } from '~/api/prime'
 
+import Button from '~/components/material/Button'
 import IconButton from '~/components/material/IconButton'
 import TopAppBar from '~/components/material/TopAppBar'
+import { getDeviceName } from '~/utils/device'
 
 type PrimeActivityProps = {
   dongleId: string
 }
 
 const PrimeActivity: VoidComponent<PrimeActivityProps> = (props) => {
+  const dongleId = () => props.dongleId
+  // const [subscription] = createResource(dongleId, getSubscriptionStatus)
+
+  const [device] = createResource(dongleId, getDevice)
+  const [subscribeInfo] = createResource(dongleId, getSubscribeInfo)
+
   return (
-    <div>
-      <TopAppBar leading={<IconButton href={`/${props.dongleId}`}>arrow_back</IconButton>}>
+    <div class="prose grid gap-4 px-4">
+      <TopAppBar leading={<IconButton href={`/${dongleId()}`}>arrow_back</IconButton>}>
         Device settings
       </TopAppBar>
+
+      <Show when={device()} keyed>{device => <>
+        Device: {getDeviceName(device)} ({device.dongle_id})
+      </>}</Show>
+
+      <h2 class="text-headline-md">comma prime</h2>
+
+      <Switch>
+        <Match when={subscribeInfo.loading}>
+          Loading...
+        </Match>
+
+        <Match when={!subscribeInfo()?.has_prime}>
+          <span>No prime</span>
+        </Match>
+
+        <Match when={subscribeInfo()?.has_prime}>
+          <span>Device: Name (dongle id)</span>
+          <span>Plan: Lite (without data plan)</span>
+          <span>Joined: March 1, 2024</span>
+          <span>Next payment: July 31, 2024</span>
+          <span>Amount: $10.00</span>
+
+          <Button>Update payment method</Button>
+          <Button>Cancel subscription</Button>
+        </Match>
+      </Switch>
     </div>
   )
 }
