be540c16bb6264732d8c43c4f8d881a46f36a995
Cameron Clough <cameronjclough@gmail.com>
2024-07-12 00:48:28 +0100

    fetch subscribe_info or subscription depending on prime status

--- src/pages/dashboard/activities/PrimeActivity.tsx
+++ src/pages/dashboard/activities/PrimeActivity.tsx
@@ -1,22 +1,49 @@
-import { createResource, Match, Show, Switch, type VoidComponent } from 'solid-js'
+import { createResource, Match, Show, Suspense, Switch, type VoidComponent } from 'solid-js'
+
 import { getDevice } from '~/api/devices'
-import { getSubscribeInfo } from '~/api/prime'
+import { getSubscribeInfo, getSubscriptionStatus } from '~/api/prime'
+import type { Device } from '~/types'
+import { getDeviceName } from '~/utils/device'
 
 import Button from '~/components/material/Button'
 import IconButton from '~/components/material/IconButton'
 import TopAppBar from '~/components/material/TopAppBar'
-import { getDeviceName } from '~/utils/device'
 
 type PrimeActivityProps = {
   dongleId: string
 }
 
+const SubscribeInfo: VoidComponent<{ device: Device }> = (props) => {
+  const [subscribeInfo] = createResource(() => props.device.dongle_id, getSubscribeInfo)
+  return <>
+    Trial end (no data): {subscribeInfo()?.trial_end_nodata}
+  </>
+}
+
+const currency = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' })
+
+const Subscription: VoidComponent<{ device: Device }> = (props) => {
+  const [subscription] = createResource(() => props.device.dongle_id, getSubscriptionStatus)
+  const [amount] = createResource(subscription, (sub) => currency.format(sub.amount / 100))
+
+  return <>
+    <ul>
+      <li>Plan: Lite (without data plan)</li>
+      <li>Joined: March 1, 2024</li>
+      <li>Next payment: July 31, 2024</li>
+      <li>Amount: {amount()}</li>
+    </ul>
+
+    <div class="flex gap-4">
+      <Button>Update payment method</Button>
+      <Button>Cancel subscription</Button>
+    </div>
+  </>
+}
+
 const PrimeActivity: VoidComponent<PrimeActivityProps> = (props) => {
   const dongleId = () => props.dongleId
-  // const [subscription] = createResource(dongleId, getSubscriptionStatus)
-
   const [device] = createResource(dongleId, getDevice)
-  const [subscribeInfo] = createResource(dongleId, getSubscribeInfo)
 
   return (
     <div class="prose grid gap-4 px-4">
@@ -30,26 +57,17 @@ const PrimeActivity: VoidComponent<PrimeActivityProps> = (props) => {
 
       <h2 class="text-headline-md">comma prime</h2>
 
-      <Switch>
-        <Match when={subscribeInfo.loading}>
-          Loading...
-        </Match>
+      <Suspense fallback="Loading...">
+        <Switch>
+          <Match when={device()?.prime === true}>
+            <Subscription device={device()!} />
+          </Match>
 
-        <Match when={!subscribeInfo()?.has_prime}>
-          <span>No prime</span>
-        </Match>
-
-        <Match when={subscribeInfo()?.has_prime}>
-          <span>Device: Name (dongle id)</span>
-          <span>Plan: Lite (without data plan)</span>
-          <span>Joined: March 1, 2024</span>
-          <span>Next payment: July 31, 2024</span>
-          <span>Amount: $10.00</span>
-
-          <Button>Update payment method</Button>
-          <Button>Cancel subscription</Button>
-        </Match>
-      </Switch>
+          <Match when={device()?.prime === false}>
+            <SubscribeInfo device={device()!} />
+          </Match>
+        </Switch>
+      </Suspense>
     </div>
   )
 }
