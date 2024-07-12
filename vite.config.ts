cd28398076d21087d8f2bd2d45dca67fb52fac80
Sean Aguinaga <aguinaga.sean@gmail.com>
2024-07-11 19:21:19 -0700

    works?

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,40 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
 import devtools from 'solid-devtools/vite'
+import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
+import solid from 'vite-plugin-solid'
 
 export default defineConfig({
   plugins: [
     devtools(),
     solid({
       ssr: false,
+    }),
+    VitePWA({
+      base: '/',
+
+      registerType: 'autoUpdate',
+      injectRegister: false,
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        name: 'Connect',
+        short_name: 'Connect',
+        description: 'Connect for Comma AI',
+        start_url: '/',
+      },
+
+      workbox: undefined,
+
+      devOptions: {
+        enabled: true,
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+        type: 'module',
+      },
     }),
   ],
   server: {
