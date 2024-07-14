8762357dd6dfc39c011c0f4d6a62939a90a31689
Trent Rand <contact@trentrand.com>
2024-07-13 22:38:37 -0700

    Deprecate use of RouteSegments segment times fields (#77)

--- src/types.d.ts
+++ src/types.d.ts
@@ -92,9 +92,6 @@ export interface RouteShareSignature extends Record<string, string> {
 export interface RouteSegments extends Route {
   end_time_utc_millis: number
   is_preserved: boolean
-  segment_end_times: number[]
-  segment_numbers: number[]
-  segment_start_times: number[]
   share_exp: RouteShareSignature['exp']
   share_sig: RouteShareSignature['sig']
   start_time_utc_millis: number
