d4b0fb218788d25fc04a64b27f03616816d30a81
Cameron Clough <cameronjclough@gmail.com>
2024-07-17 01:33:38 +0100

    cleanup device sorting

--- src/api/devices.ts
+++ src/api/devices.ts
@@ -2,19 +2,17 @@ import type { Device, DrivingStatistics } from '~/types'
 
 import { fetcher } from '.'
 
-const sortDevices = (devices: Device[]) => {
-  return devices.sort((a, b) => {
-    if (a.is_owner !== b.is_owner) {
-      return a.is_owner ? -1 : 1
-    } else if (a.alias && b.alias) {
-      return a.alias.localeCompare(b.alias)
-    } else if (!a.alias && !b.alias) {
-      return a.dongle_id.localeCompare(b.dongle_id)
-    } else {
-      return a.alias ? -1 : 1
-    }
-  })
-}
+const sortDevices = (devices: Device[]) => devices.sort((a, b) => {
+  if (a.is_owner !== b.is_owner) {
+    return a.is_owner ? -1 : 1
+  } else if (a.alias && b.alias) {
+    return a.alias.localeCompare(b.alias)
+  } else if (!a.alias && !b.alias) {
+    return a.dongle_id.localeCompare(b.dongle_id)
+  } else {
+    return a.alias ? -1 : 1
+  }
+})
 
 export const getDevice = async (dongleId: string) =>
   fetcher<Device>(`/v1.1/devices/${dongleId}/`)
@@ -24,5 +22,5 @@ export const getDeviceStats = async (dongleId: string) =>
 
 export const getDevices = async () =>
   fetcher<Device[]>('/v1/me/devices/')
-    .then(devices => sortDevices(devices))
+    .then(sortDevices)
     .catch(() => [])
