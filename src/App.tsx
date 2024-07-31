dbde7d2008d63b2c440020e979d0bcd96088e238
griffinbaker12 <96966609+griffinbaker12@users.noreply.github.com>
2024-07-31 17:01:07 -0500

    inlude assets and offline view

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Suspense, lazy, type VoidComponent } from 'solid-js'
 import { Router, Route } from '@solidjs/router'
+import OfflineIndicator from './components/Offline'
 
 const Login = lazy(() => import('./pages/auth/login'))
 const Logout = lazy(() => import('./pages/auth/logout'))
@@ -9,13 +10,15 @@ const Dashboard = lazy(() => import('./pages/dashboard'))
 
 const App: VoidComponent = () => {
   return (
-    <Router root={(props) => <Suspense>{props.children}</Suspense>}>
-      <Route path="/login" component={Login} />
-      <Route path="/logout" component={Logout} />
-      <Route path="/auth" component={Auth} />
+    <OfflineIndicator>
+      <Router root={(props) => <Suspense>{props.children}</Suspense>}>
+        <Route path="/login" component={Login} />
+        <Route path="/logout" component={Logout} />
+        <Route path="/auth" component={Auth} />
 
-      <Route path="/*dongleId" component={Dashboard} />
-    </Router>
+        <Route path="/*dongleId" component={Dashboard} />
+      </Router>
+    </OfflineIndicator>
   )
 }
 
