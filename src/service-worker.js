236d72768816333b603cbb70680a87f158ec4ab7
Som Gupta <78577376+knownotunknown@users.noreply.github.com>
2024-10-19 16:27:37 -0500

    test: further narrowing down point of failure

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,64 +1,64 @@
-const SERVICE_WORKER_VERSION = 1;
-const CACHE_NAME = `comma-connect-cache-v${SERVICE_WORKER_VERSION}`;
+// const SERVICE_WORKER_VERSION = 1;
+// const CACHE_NAME = `comma-connect-cache-v${SERVICE_WORKER_VERSION}`;
 
-const STATIC_ASSETS = [
-  '/index.html',
-  '/manifest.json',
-  '/no-connection.html',
-  '/images/icon-256.png',
-];
+// const STATIC_ASSETS = [
+//   '/index.html',
+//   '/manifest.json',
+//   '/no-connection.html',
+//   '/images/icon-256.png',
+// ];
 
-const CACHEABLE_URL_PATTERNS = [
-  /\/images\/.*\.(png|jpg|svg)$/,
-  /\/fonts\.googleapis\.com\//,
-  /\/fonts\.gstatic\.com\//,
-];
+// const CACHEABLE_URL_PATTERNS = [
+//   /\/images\/.*\.(png|jpg|svg)$/,
+//   /\/fonts\.googleapis\.com\//,
+//   /\/fonts\.gstatic\.com\//,
+// ];
 
-self.addEventListener('install', (installEvent) => {
-  installEvent.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(STATIC_ASSETS);
-    })
-  );
-});
+// self.addEventListener('install', (installEvent) => {
+//   installEvent.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => {
+//       return cache.addAll(STATIC_ASSETS);
+//     })
+//   );
+// });
 
-self.addEventListener('fetch', (fetchEvent) => {
-  if (fetchEvent.request.method !== 'GET') {
-    return;
-  }
+// self.addEventListener('fetch', (fetchEvent) => {
+//   if (fetchEvent.request.method !== 'GET') {
+//     return;
+//   }
 
-  if (shouldCacheUrl(fetchEvent.request.url)) {
-    fetchEvent.respondWith(
-      caches.match(fetchEvent.request).then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
-        return fetch(fetchEvent.request).then((networkResponse) => {
-          if (networkResponse.status === 200) {
-            const clonedResponse = networkResponse.clone();
-            caches.open(CACHE_NAME).then((cache) => {
-              cache.put(fetchEvent.request, clonedResponse);
-            });
-          }
-          return networkResponse;
-        });
-      }).catch(handleFetchError)
-    );
-  } else {
-    fetchEvent.respondWith(
-      fetch(fetchEvent.request).catch(handleFetchError)
-    );
-  }
-});
+//   if (shouldCacheUrl(fetchEvent.request.url)) {
+//     fetchEvent.respondWith(
+//       caches.match(fetchEvent.request).then((cachedResponse) => {
+//         if (cachedResponse) {
+//           return cachedResponse;
+//         }
+//         return fetch(fetchEvent.request).then((networkResponse) => {
+//           if (networkResponse.status === 200) {
+//             const clonedResponse = networkResponse.clone();
+//             caches.open(CACHE_NAME).then((cache) => {
+//               cache.put(fetchEvent.request, clonedResponse);
+//             });
+//           }
+//           return networkResponse;
+//         });
+//       }).catch(handleFetchError)
+//     );
+//   } else {
+//     fetchEvent.respondWith(
+//       fetch(fetchEvent.request).catch(handleFetchError)
+//     );
+//   }
+// });
 
-function handleFetchError(error) {
-  if (!navigator.onLine) {
-    return caches.match('/no-connection.html');
-  }
-  throw error;
-}
+// function handleFetchError(error) {
+//   if (!navigator.onLine) {
+//     return caches.match('/no-connection.html');
+//   }
+//   throw error;
+// }
 
-function shouldCacheUrl(url) {
-  const urlPath = new URL(url).pathname;
-  return CACHEABLE_URL_PATTERNS.some((pattern) => pattern.test(urlPath));
-}
+// function shouldCacheUrl(url) {
+//   const urlPath = new URL(url).pathname;
+//   return CACHEABLE_URL_PATTERNS.some((pattern) => pattern.test(urlPath));
+// }
--- src/utils/register-service-worker.ts
+++ src/utils/register-service-worker.ts
@@ -1,14 +1,14 @@
 export function registerServiceWorker(): void {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-    //   navigator.serviceWorker.register('/service-worker.js')
-    //     .then(registration => {
-    //       console.log('Service Worker registered with scope:', registration.scope)
-    //     })
-    //     .catch(error => {
-    //       console.error('Service Worker registration failed:', error)
-    //     })
-      console.log('test')
+      navigator.serviceWorker.register('/service-worker.js')
+        .then(registration => {
+          console.log('Service Worker registered with scope:', registration.scope)
+        })
+        .catch(error => {
+          console.error('Service Worker registration failed:', error)
+        })
+    //   console.log('test')
     })
   }
 }
