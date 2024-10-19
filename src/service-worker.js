d127624a28a6e351a95792f068cb31cae1cbc530
Som Gupta <78577376+knownotunknown@users.noreply.github.com>
2024-10-19 17:29:14 -0500

    Update service-worker.js

--- src/service-worker.js
+++ src/service-worker.js
@@ -51,30 +51,6 @@ self.addEventListener('fetch', (fetchEvent) => {
   }
 });
 
-  // Commenting out complex caching logic
-  // if (shouldCacheUrl(fetchEvent.request.url)) {
-  //   fetchEvent.respondWith(
-  //     caches.match(fetchEvent.request).then((cachedResponse) => {
-  //       if (cachedResponse) {
-  //         return cachedResponse;
-  //       }
-  //       return fetch(fetchEvent.request).then((networkResponse) => {
-  //         if (networkResponse.status === 200) {
-  //           const clonedResponse = networkResponse.clone();
-  //           caches.open(CACHE_NAME).then((cache) => {
-  //             cache.put(fetchEvent.request, clonedResponse);
-  //           });
-  //         }
-  //         return networkResponse;
-  //       });
-  //     }).catch(handleFetchError)
-  //   );
-  // } else {
-  //   fetchEvent.respondWith(
-  //     fetch(fetchEvent.request).catch(handleFetchError)
-  //   );
-  // }
-
 function handleFetchError(error) {
   if (!navigator.onLine) {
     return caches.match('/no-connection.html');
