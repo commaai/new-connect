1ea3a9b16d267a9f50aa72265d0ce8d1f97bbab3
Som Gupta <78577376+knownotunknown@users.noreply.github.com>
2024-10-19 16:40:26 -0500

    Update service-worker.js

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,6 +1,7 @@
-// const SERVICE_WORKER_VERSION = 1;
-// const CACHE_NAME = `comma-connect-cache-v${SERVICE_WORKER_VERSION}`;
+const SERVICE_WORKER_VERSION = 1;
+const CACHE_NAME = `comma-connect-cache-v${SERVICE_WORKER_VERSION}`;
 
+// Commenting out static assets as they might not exist or have different paths
 // const STATIC_ASSETS = [
 //   '/index.html',
 //   '/manifest.json',
@@ -8,56 +9,64 @@
 //   '/images/icon-256.png',
 // ];
 
+// Commenting out cacheable URL patterns to simplify caching logic
 // const CACHEABLE_URL_PATTERNS = [
 //   /\/images\/.*\.(png|jpg|svg)$/,
 //   /\/fonts\.googleapis\.com\//,
 //   /\/fonts\.gstatic\.com\//,
 // ];
 
-// self.addEventListener('install', (installEvent) => {
-//   installEvent.waitUntil(
-//     caches.open(CACHE_NAME).then((cache) => {
-//       return cache.addAll(STATIC_ASSETS);
-//     })
-//   );
-// });
+self.addEventListener('install', (installEvent) => {
+  // Commenting out the caching of static assets
+  // installEvent.waitUntil(
+  //   caches.open(CACHE_NAME).then((cache) => {
+  //     return cache.addAll(STATIC_ASSETS);
+  //   })
+  // );
+});
 
-// self.addEventListener('fetch', (fetchEvent) => {
-//   if (fetchEvent.request.method !== 'GET') {
-//     return;
-//   }
+self.addEventListener('fetch', (fetchEvent) => {
+  if (fetchEvent.request.method !== 'GET') {
+    return;
+  }
 
-//   if (shouldCacheUrl(fetchEvent.request.url)) {
-//     fetchEvent.respondWith(
-//       caches.match(fetchEvent.request).then((cachedResponse) => {
-//         if (cachedResponse) {
-//           return cachedResponse;
-//         }
-//         return fetch(fetchEvent.request).then((networkResponse) => {
-//           if (networkResponse.status === 200) {
-//             const clonedResponse = networkResponse.clone();
-//             caches.open(CACHE_NAME).then((cache) => {
-//               cache.put(fetchEvent.request, clonedResponse);
-//             });
-//           }
-//           return networkResponse;
-//         });
-//       }).catch(handleFetchError)
-//     );
-//   } else {
-//     fetchEvent.respondWith(
-//       fetch(fetchEvent.request).catch(handleFetchError)
-//     );
-//   }
-// });
+  // Simplifying fetch event handling to just pass through all requests
+  fetchEvent.respondWith(fetch(fetchEvent.request).catch(handleFetchError));
 
-// function handleFetchError(error) {
-//   if (!navigator.onLine) {
-//     return caches.match('/no-connection.html');
-//   }
-//   throw error;
-// }
+  // Commenting out complex caching logic
+  // if (shouldCacheUrl(fetchEvent.request.url)) {
+  //   fetchEvent.respondWith(
+  //     caches.match(fetchEvent.request).then((cachedResponse) => {
+  //       if (cachedResponse) {
+  //         return cachedResponse;
+  //       }
+  //       return fetch(fetchEvent.request).then((networkResponse) => {
+  //         if (networkResponse.status === 200) {
+  //           const clonedResponse = networkResponse.clone();
+  //           caches.open(CACHE_NAME).then((cache) => {
+  //             cache.put(fetchEvent.request, clonedResponse);
+  //           });
+  //         }
+  //         return networkResponse;
+  //       });
+  //     }).catch(handleFetchError)
+  //   );
+  // } else {
+  //   fetchEvent.respondWith(
+  //     fetch(fetchEvent.request).catch(handleFetchError)
+  //   );
+  // }
+});
 
+function handleFetchError(error) {
+  // Commenting out no-connection handling as the file might not exist
+  // if (!navigator.onLine) {
+  //   return caches.match('/no-connection.html');
+  // }
+  throw error;
+}
+
+// Commenting out this function as it's not being used now
 // function shouldCacheUrl(url) {
 //   const urlPath = new URL(url).pathname;
 //   return CACHEABLE_URL_PATTERNS.some((pattern) => pattern.test(urlPath));
