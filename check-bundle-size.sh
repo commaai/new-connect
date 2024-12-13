db219cd8cb9f3a7d73f73562178e598f44fa886d
Leo Forney <forneyleo@gmail.com>
2024-12-13 09:20:32 -0600

    Match bundle size with README

--- check-bundle-size.sh
+++ check-bundle-size.sh
@@ -8,12 +8,12 @@ gzip -r -9 dist
 BUNDLE_SIZE=$(du -sk dist | cut -f1)
 echo "Bundle size is $BUNDLE_SIZE K"
 
-if [ $BUNDLE_SIZE -lt 200 ]; then
+if [ $BUNDLE_SIZE -lt 400 ]; then
   echo "Bundle sizer lower than expected, let's lower the limit!"
   exit 1
 fi
 
-if [ $BUNDLE_SIZE -gt 325 ]; then
+if [ $BUNDLE_SIZE -gt 500 ]; then
   echo "Exceeded bundle size limit!"
   exit 1
 fi
