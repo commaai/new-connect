56e68a6e32be26262427ceef1cfa55bcf7fdfee5
Leo Forney <forneyleo@gmail.com>
2024-12-13 09:22:17 -0600

    Lower lower bundle size

--- check-bundle-size.sh
+++ check-bundle-size.sh
@@ -8,7 +8,7 @@ gzip -r -9 dist
 BUNDLE_SIZE=$(du -sk dist | cut -f1)
 echo "Bundle size is $BUNDLE_SIZE K"
 
-if [ $BUNDLE_SIZE -lt 400 ]; then
+if [ $BUNDLE_SIZE -lt 200 ]; then
   echo "Bundle sizer lower than expected, let's lower the limit!"
   exit 1
 fi
