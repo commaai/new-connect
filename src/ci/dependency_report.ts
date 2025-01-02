3219342b2363fc1d725550a2be7549e465269c71
Cameron Clough <cameronjclough@gmail.com>
2025-01-02 15:30:54 +0000

    refactor: CI dependency report
    - don't delete dist folder after running
    - rewrite in typescript and enable linting

---
+++ src/ci/dependency_report.ts
@@ -0,0 +1,20 @@
+import fs from 'node:fs'
+
+const bundle = process.argv[2]
+const dir = bundle + '/assets/'
+const extension = '.map'
+const mapFiles = fs.readdirSync(dir).filter(fn => fn.endsWith(extension))
+
+const report = []
+for (const mapFile of mapFiles) {
+  const mapFileData = JSON.parse(fs.readFileSync(dir + mapFile, 'utf8')) as { sources: string[] }
+  const assetFile = mapFile.replace(extension, '')
+  const sources = mapFileData.sources.filter(source => source.includes('node_modules')).map(source => source.split('node_modules/')[1])
+  report.push({ 'asset': assetFile, 'size': fs.statSync(dir + assetFile).size, 'sources': sources})
+}
+report.sort((b, a) => a.size - b.size).forEach(entry => {
+  console.log(entry.asset + ':' + (entry.size / 1024).toFixed(2) + 'KB')
+  for (const source of entry.sources) {
+    console.log('   ' + source)
+  }
+})
