28efcf2c46b23da0f24ce8cc39fad8e9ee09e9b6
Cameron Clough <cameronjclough@gmail.com>
2025-01-31 20:31:22 +0000

    fix(bundle-size): measure file size not disk usage (#147)

--- check-bundle-size.sh
+++ check-bundle-size.sh
@@ -4,16 +4,15 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd $DIR
 
-gzip -r -9 dist
-BUNDLE_SIZE=$(du -sk dist | cut -f1)
-echo "Bundle size is $BUNDLE_SIZE K"
+BUNDLE_SIZE=$(find dist -type f ! -name "*.map" -exec sh -c 'gzip -9c "{}" | wc -c' \; | awk '{total += $1} END {print int(total/1024)}')
+echo "Bundle size is ${BUNDLE_SIZE}KiB"
 
 if [ $BUNDLE_SIZE -lt 200 ]; then
   echo "Bundle sizer lower than expected, let's lower the limit!"
   exit 1
 fi
 
-if [ $BUNDLE_SIZE -gt 325 ]; then
+if [ $BUNDLE_SIZE -gt 235 ]; then
   echo "Exceeded bundle size limit!"
   exit 1
 fi
