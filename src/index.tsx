35ea895d6123c0c7e194c56856474a5812c634a5
Cameron Clough <cameronjclough@gmail.com>
2025-01-31 22:39:48 +0000

    update dependencies (#149)

--- src/index.tsx
+++ src/index.tsx
@@ -6,12 +6,11 @@ import { render } from 'solid-js/web'
 import App from './App'
 
 const environment = import.meta.env.VITE_SENTRY_ENVIRONMENT as string | undefined
-if (environment) {
-  Sentry.init({
-    dsn: 'https://c3402db23a1a02fe83b7a43b7dbbbac0@o33823.ingest.us.sentry.io/4508738328854529',
-    environment,
-  })
-}
+Sentry.init({
+  enabled: !!environment,
+  dsn: 'https://c3402db23a1a02fe83b7a43b7dbbbac0@o33823.ingest.us.sentry.io/4508738328854529',
+  environment,
+})
 
 const root = document.getElementById('root')
 
