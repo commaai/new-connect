c293446427e7f48754a2eba6ae15eefe88337efb
california <dcr@californias-Mac-mini.local>
2025-02-12 23:29:03 -0800

    rm duplicate code

--- src/components/RouteInfo.tsx
+++ src/components/RouteInfo.tsx
@@ -1,18 +1,7 @@
-import { createResource, Suspense, type VoidComponent } from 'solid-js'
+import type { VoidComponent } from 'solid-js'
 import clsx from 'clsx'
 import type { Route } from '~/types'
-import { TimelineStatistics, getTimelineStatistics } from '~/api/derived'
-import { formatDistance, formatRouteDuration } from '~/utils/format'
-
-const formatEngagement = (timeline?: TimelineStatistics): string => {
-  if (!timeline) return ''
-  const { engagedDuration, duration } = timeline
-  return `${(100 * (engagedDuration / duration)).toFixed(0)}%`
-}
-
-const formatUserFlags = (timeline?: TimelineStatistics): string => {
-  return timeline?.userFlags.toString() ?? ''
-}
+import RouteStatistics from './RouteStatistics'
 
 type RouteInfoProps = {
   class?: string
@@ -20,43 +9,9 @@ type RouteInfoProps = {
 }
 
 const RouteInfo: VoidComponent<RouteInfoProps> = (props) => {
-  const [timeline] = createResource(() => props.route, getTimelineStatistics)
-
   return (
     <div class={clsx('flex flex-col rounded-t-md bg-surface-container-low p-5', props.class)}>
-      <div class="flex size-full items-stretch gap-8">
-        <div class="flex grow flex-col justify-between">
-          <span class="text-body-sm text-on-surface-variant">Distance</span>
-          <span class="font-mono text-label-lg uppercase">
-            {formatDistance(props.route?.length)}
-          </span>
-        </div>
-
-        <div class="flex grow flex-col justify-between">
-          <span class="text-body-sm text-on-surface-variant">Duration</span>
-          <span class="font-mono text-label-lg uppercase">
-            {formatRouteDuration(props.route)}
-          </span>
-        </div>
-
-        <div class="hidden grow flex-col justify-between xs:flex">
-          <span class="text-body-sm text-on-surface-variant">Engaged</span>
-          <Suspense>
-            <span class="font-mono text-label-lg uppercase">
-              {formatEngagement(timeline())}
-            </span>
-          </Suspense>
-        </div>
-
-        <div class="flex grow flex-col justify-between">
-          <span class="text-body-sm text-on-surface-variant">User flags</span>
-          <Suspense>
-            <span class="font-mono text-label-lg uppercase">
-              {formatUserFlags(timeline())}
-            </span>
-          </Suspense>
-        </div>
-      </div>
+      <RouteStatistics route={props.route} />
     </div>
   )
 }
