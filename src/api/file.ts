1ace31045b87d06f73598aad3996c24c559915ba
Cameron Clough <cameronjclough@gmail.com>
2025-03-22 20:49:26 +0000

    not required

--- src/api/file.ts
+++ src/api/file.ts
@@ -7,8 +7,5 @@ export const getAlreadyUploadedFiles = (routeName: Route['fullname']): Promise<F
 export const requestToUploadFiles = (dongleId: string, paths: string[], expiryDays: number = 7) =>
   fetcher<UploadFileMetadataResponse>(`/v1/${dongleId}/upload_urls/`, {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
     body: JSON.stringify({ expiry_days: expiryDays, paths }),
   })
