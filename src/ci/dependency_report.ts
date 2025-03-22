3b65e89baa6f786279211afbca55069119d67603
Trey Moen <50057480+greatgitsby@users.noreply.github.com>
2025-03-22 04:13:16 -0700

    feat/fix: CI script improvements running locally (#217)
    [fix: check lines works locally, also removes extra
    /](https://github.com/commaai/connect/commit/a347e4128bd38e2e5053bc3718d78a5af8e9a610)
    
    [fix: remove extra / from bundle
    size](https://github.com/commaai/connect/commit/a0c269862b962a8bd93b2766e2a026a49a4514b3)
    
    [feat: add unminified dependency file to dependency
    report](https://github.com/commaai/connect/commit/24419382782909a244584d1341023584bd5d479b)
    (nice for checking weight of dependency)

--- src/ci/dependency_report.ts
+++ src/ci/dependency_report.ts
@@ -9,13 +9,17 @@ const report = []
 for (const mapFile of mapFiles) {
   const mapFileData = JSON.parse(fs.readFileSync(dir + mapFile, 'utf8')) as { sources: string[] }
   const assetFile = mapFile.replace(extension, '')
-  const sources = mapFileData.sources.filter(source => source.includes('node_modules')).map(source => source.split('node_modules/')[1])
+  const sources = mapFileData.sources
+    .filter(source => source.includes('node_modules'))
+    .map(source => ({
+      name: source.split('node_modules/')[1],
+      size: fs.statSync(dir + source).size
+    }))
   report.push({ 'asset': assetFile, 'size': fs.statSync(dir + assetFile).size, 'sources': sources})
-  fs.unlinkSync(dir + mapFile)
 }
 report.sort((b, a) => a.size - b.size).forEach(entry => {
-  console.log(entry.asset + ':' + (entry.size / 1024).toFixed(2) + 'KB')
+  console.log((entry.size / 1024).toFixed(2) + 'KB (minified): ' + entry.asset)
   for (const source of entry.sources) {
-    console.log('   ' + source)
+    console.log('   ' + (source.size / 1024).toFixed(2) + 'KB (uncompressed): ' + source.name)
   }
 })
