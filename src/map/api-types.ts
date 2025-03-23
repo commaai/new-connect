b55fb50100fad8a9e3555724b0267c735e5e7f0e
Cameron Clough <cameronjclough@gmail.com>
2025-03-23 13:17:45 +0000

    RouteList: add place names (#226)

--- src/map/api-types.ts
+++ src/map/api-types.ts
@@ -1,7 +1,7 @@
 import type { FeatureCollection, Point } from 'geojson'
 
 /**
- * @see https://docs.mapbox.com/api/search/geocoding/#geocoding-response-object
+ * @see {@link https://docs.mapbox.com/api/search/geocoding/#geocoding-response-object}
  */
 export interface ReverseGeocodingResponse extends FeatureCollection<Point, ReverseGeocodingFeatureProperties> {
   attribution: string
@@ -10,8 +10,38 @@ export interface ReverseGeocodingResponse extends FeatureCollection<Point, Rever
 export type ReverseGeocodingFeature = ReverseGeocodingResponse['features'][number]
 
 interface ReverseGeocodingFeatureProperties {
+  feature_type: 'country' | 'region' | 'postcode' | 'district' | 'place' | 'locality' | 'neighborhood' | 'street' | 'address'
   name: string
   name_preferred: string
   place_formatted: string
   full_address: string
+  context: ReverseGeocodingContextObject
+}
+
+/**
+ * @see {@link https://docs.mapbox.com/api/search/geocoding/#the-context-object}
+ */
+interface ReverseGeocodingContextObject<S = ReverseGeocodingContextSubObject> {
+  country?: S & {
+    country_code: string
+    country_code_alpha_3: string
+  }
+  region?: S & {
+    region_code: string
+    region_code_full: string
+  }
+  postcode?: S
+  district?: S
+  place?: S
+  locality?: S
+  neighborhood?: S
+  street?: S
+  address?: S & {
+    address_number: string
+    street_name: string
+  }
+}
+
+interface ReverseGeocodingContextSubObject {
+  name: string
 }
--- src/map/geocode.spec.ts
+++ src/map/geocode.spec.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test } from 'vitest'
 
-import { getFullAddress, reverseGeocode } from './geocode'
+import { getFullAddress, getPlaceName, reverseGeocode } from './geocode'
 
 describe('reverseGeocode', () => {
   test('return null if coords are [0, 0]', async () => {
@@ -17,5 +17,19 @@ describe('getFullAddress', () => {
     // expect(await getFullAddress([-77.036574, 38.8976765])).toBe('1600 Pennsylvania Avenue Northwest, Washington, District of Columbia 20500, United States')
     expect(await getFullAddress([-0.106640, 51.514209])).toBe('133 Fleet Street, City of London, London, EC4A 2BB, United Kingdom')
     expect(await getFullAddress([-2.076843, 51.894799])).toBe('4 Montpellier Drive, Cheltenham, GL50 1TX, United Kingdom')
+  })
+})
+
+describe('getPlaceName', () => {
+  test('return null if coords are [0, 0]', async () => {
+    expect(await getPlaceName([0, 0])).toBeNull()
+  })
+
+  test('normal usage', async () => {
+    expect(await getPlaceName([-117.168638, 32.723695])).toBe('India Street')
+    expect(await getPlaceName([-118.192757, 33.763015])).toBe('Shoreline Drive')
+    expect(await getPlaceName([-0.113643, 51.504546])).toBe('A301')
+    expect(await getPlaceName([5.572254, 50.644280])).toBe('Rue Joffre')
+    expect(await getPlaceName([-2.236802, 53.480931])).toBe('Piccadilly Gardens')
   })
 })
