2044c3f582fc946142ae0becb96a9950fa1a1d30
Cameron Clough <cameronjclough@gmail.com>
2025-03-23 12:48:01 +0000

    check if device owner or superuser

--- src/pages/dashboard/activities/RouteActivity.tsx
+++ src/pages/dashboard/activities/RouteActivity.tsx
@@ -1,6 +1,8 @@
-import { createResource, createSignal, lazy, onMount, Suspense, type VoidComponent } from 'solid-js'
+import { createResource, createSignal, lazy, Suspense, type VoidComponent } from 'solid-js'
 
 import { setRouteViewed } from '~/api/athena'
+import { getDevice } from '~/api/devices'
+import { getProfile } from '~/api/profile'
 import { getRoute } from '~/api/route'
 import { dayjs } from '~/utils/format'
 
@@ -33,9 +35,11 @@ const RouteActivity: VoidComponent<RouteActivityProps> = (props) => {
     if (video) video.currentTime = newTime
   }
 
-  onMount(async () => {
-    // FIXME: don't send for shared devices, unless superuser
-    await setRouteViewed(props.dongleId, props.dateStr)
+  const [device] = createResource(() => props.dongleId, getDevice)
+  const [profile] = createResource(getProfile)
+  createResource(() => [device(), profile(), props.dateStr] as const, async ([device, profile, dateStr]) => {
+    if (!device || !profile || (!device.is_owner && !profile.superuser)) return
+    await setRouteViewed(device.dongle_id, dateStr)
   })
 
   return (
