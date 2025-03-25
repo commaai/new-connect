5c7f7f1f597c02d6c8408e784229fea855d5863a
Cameron Clough <cameronjclough@gmail.com>
2025-03-25 22:47:22 +0000

    cleanup(dashboard): fetch devices where they are used (#243)
    Instead of prop drilling, we can just fetch the list of devices where
    they are used. We should use a query library to cache/de-duplicate
    requests.

--- src/pages/dashboard/components/DeviceList.tsx
+++ src/pages/dashboard/components/DeviceList.tsx
@@ -1,7 +1,8 @@
-import { For, type VoidComponent } from 'solid-js'
+import { createResource, For, Suspense, type VoidComponent } from 'solid-js'
 import { useLocation } from '@solidjs/router'
 import clsx from 'clsx'
 
+import { getDevices } from '~/api/devices'
 import { useDrawerContext } from '~/components/material/Drawer'
 import List, { ListItem, ListItemContent } from '~/components/material/List'
 import type { Device } from '~/types'
@@ -10,7 +11,6 @@ import storage from '~/utils/storage'
 
 type DeviceListProps = {
   class?: string
-  devices: Device[]
 }
 
 const DeviceList: VoidComponent<DeviceListProps> = (props) => {
@@ -23,11 +23,12 @@ const DeviceList: VoidComponent<DeviceListProps> = (props) => {
     storage.setItem('lastSelectedDongleId', device.dongle_id)
   }
 
+  const [devices] = createResource(getDevices)
   return (
     <List variant="nav" class={props.class}>
-      <For each={props.devices}>
-        {(device) => {
-          return (
+      <Suspense fallback={<div class="h-14 skeleton-loader rounded-xl" />}>
+        <For each={devices()}>
+          {(device) => (
             <ListItem
               variant="nav"
               leading={<div class={clsx('m-2 size-2 shrink-0 rounded-full', deviceIsOnline(device) ? 'bg-green-400' : 'bg-gray-400')} />}
@@ -41,9 +42,9 @@ const DeviceList: VoidComponent<DeviceListProps> = (props) => {
                 subhead={<span class="font-mono text-label-sm lowercase">{device.dongle_id}</span>}
               />
             </ListItem>
-          )
-        }}
-      </For>
+          )}
+        </For>
+      </Suspense>
     </List>
   )
 }
