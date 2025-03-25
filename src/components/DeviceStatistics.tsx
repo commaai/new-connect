7a114714e2c773b919e407e71ec88b3547c0c44e
Trey Moen <trey@moen.ai>
2025-03-24 19:33:18 -0700

    more inlining

--- src/components/DeviceStatistics.tsx
+++ src/components/DeviceStatistics.tsx
@@ -1,36 +1,23 @@
 import { createResource } from 'solid-js'
 import type { VoidComponent } from 'solid-js'
-import clsx from 'clsx'
 
 import { getDeviceStats } from '~/api/devices'
 import { formatDistance, formatDuration } from '~/utils/format'
+import StatisticBar from './StatisticBar'
 
-type DeviceStatisticsProps = {
-  class?: string
-  dongleId: string
-}
-
-const DeviceStatistics: VoidComponent<DeviceStatisticsProps> = (props) => {
+const DeviceStatistics: VoidComponent<{ class?: string; dongleId: string }> = (props) => {
   const [statistics] = createResource(() => props.dongleId, getDeviceStats)
   const allTime = () => statistics()?.all
 
   return (
-    <div class={clsx('flex h-10 w-full', props.class)}>
-      <div class="flex grow flex-col justify-between">
-        <span class="text-body-sm text-on-surface-variant">Distance</span>
-        <span class="font-mono text-label-lg uppercase">{formatDistance(allTime()?.distance)}</span>
-      </div>
-
-      <div class="flex grow flex-col justify-between">
-        <span class="text-body-sm text-on-surface-variant">Duration</span>
-        <span class="font-mono text-label-lg uppercase">{formatDuration(allTime()?.minutes)}</span>
-      </div>
-
-      <div class="flex grow flex-col justify-between">
-        <span class="text-body-sm text-on-surface-variant">Routes</span>
-        <span class="font-mono text-label-lg uppercase">{allTime()?.routes ?? 0}</span>
-      </div>
-    </div>
+    <StatisticBar
+      class={props.class}
+      statistics={[
+        { label: 'Distance', value: formatDistance(allTime()?.distance) },
+        { label: 'Duration', value: formatDuration(allTime()?.minutes) },
+        { label: 'Routes', value: allTime()?.routes?.toString() },
+      ]}
+    />
   )
 }
 
---
+++ src/components/StatisticBar.tsx
@@ -0,0 +1,26 @@
+import clsx from 'clsx'
+import { For, Show, VoidComponent } from 'solid-js'
+
+const StatisticBar: VoidComponent<{ class?: string; statistics: { label: string; value: unknown }[] }> = (props) => {
+  return (
+    <div class="flex flex-col">
+      <div class={clsx('flex h-auto w-full justify-between gap-8', props.class)}>
+        <For each={props.statistics}>
+          {(statistic) => (
+            <div class="flex basis-0 grow flex-col justify-between">
+              <span class="text-body-sm text-on-surface-variant">{statistic.label}</span>
+              <Show
+                when={statistic.value !== undefined}
+                fallback={<div class="w-auto skeleton-loader bg-surface-container-low rounded-sm" />}
+              >
+                <span class="font-mono text-label-lg uppercase">{statistic.value?.toString()}</span>
+              </Show>
+            </div>
+          )}
+        </For>
+      </div>
+    </div>
+  )
+}
+
+export default StatisticBar
