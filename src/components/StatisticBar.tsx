9ac9b56b85cbca3eef936028a7d07590d3987c7e
Trey Moen <50057480+greatgitsby@users.noreply.github.com>
2025-03-25 09:40:27 -0700

    refactor: create StatisticsBar (#239)
    Would love this StatisticBar for
    https://github.com/commaai/connect/pull/203/ to reuse spacing and
    styling
    
    also inlined a few types to reduce LoC

---
+++ src/components/StatisticBar.tsx
@@ -0,0 +1,23 @@
+import clsx from 'clsx'
+import { For, Suspense, VoidComponent } from 'solid-js'
+
+const StatisticBar: VoidComponent<{ class?: string; statistics: { label: string; value: () => unknown }[] }> = (props) => {
+  return (
+    <div class="flex flex-col">
+      <div class={clsx('flex h-auto w-full justify-between gap-8', props.class)}>
+        <For each={props.statistics}>
+          {(statistic) => (
+            <div class="flex basis-0 grow flex-col justify-between">
+              <span class="text-body-sm text-on-surface-variant">{statistic.label}</span>
+              <Suspense fallback={<div class="h-[20px] w-auto skeleton-loader rounded-xs" />}>
+                <span class="font-mono text-label-lg uppercase">{statistic.value()?.toString() ?? '—'}</span>
+              </Suspense>
+            </div>
+          )}
+        </For>
+      </div>
+    </div>
+  )
+}
+
+export default StatisticBar
