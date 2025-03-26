44de4e85b7c587808b0d1efbbf24785c25101023
Trey Moen <trey@moen.ai>
2025-03-25 17:42:03 -0700

    switch-match, less nesting

--- src/components/upload-queue/QueueItemTable.tsx
+++ src/components/upload-queue/QueueItemTable.tsx
@@ -1,5 +1,5 @@
 import { Component, Suspense } from 'solid-js'
-import { For, Show } from 'solid-js'
+import { For, Match, Switch } from 'solid-js'
 import { Transition, TransitionGroup } from 'solid-transition-group'
 
 import Icon from '~/components/material/Icon'
@@ -33,24 +33,20 @@ const QueueItemTable: Component<{ items?: () => UploadItem[] | undefined; error?
             </div>
           }
         >
-          <Show
-            when={!(props.offline && props.items?.()?.length === 0)}
-            fallback={
+          <Switch>
+            <Match when={props.offline}>
               <div class="flex items-center justify-center h-full gap-2 text-on-surface-variant absolute inset-0">
                 <Icon name="signal_disconnected" />
                 <span>{props.error?.()}</span>
               </div>
-            }
-          >
-            <Show
-              when={props.items?.()?.length !== 0}
-              fallback={
-                <div class="flex items-center justify-center h-full gap-2 text-on-surface-variant absolute inset-0">
-                  <Icon name="cloud_done" />
-                  <span>No files in queue</span>
-                </div>
-              }
-            >
+            </Match>
+            <Match when={props.items?.()?.length === 0}>
+              <div class="flex items-center justify-center h-full gap-2 text-on-surface-variant absolute inset-0">
+                <Icon name="cloud_done" />
+                <span>No files in queue</span>
+              </div>
+            </Match>
+            <Match when={true}>
               <div class="absolute inset-0 overflow-y-auto hide-scrollbar">
                 <TransitionGroup name="list" {...animations(true)}>
                   <For each={props.items?.()}>
@@ -62,8 +58,8 @@ const QueueItemTable: Component<{ items?: () => UploadItem[] | undefined; error?
                   </For>
                 </TransitionGroup>
               </div>
-            </Show>
-          </Show>
+            </Match>
+          </Switch>
         </Suspense>
       </Transition>
     </div>
