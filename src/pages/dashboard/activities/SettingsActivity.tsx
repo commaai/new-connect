7e6abb10196b7b0b205aecc5591e247db7cb34f8
Cameron Clough <cameronjclough@gmail.com>
2025-03-26 16:32:37 +0000

    feat: add device unpair button

--- src/pages/dashboard/activities/SettingsActivity.tsx
+++ src/pages/dashboard/activities/SettingsActivity.tsx
@@ -21,6 +21,7 @@ import Icon from '~/components/material/Icon'
 import IconButton from '~/components/material/IconButton'
 import TopAppBar from '~/components/material/TopAppBar'
 import { createQuery } from '~/utils/createQuery'
+import { getDeviceName } from '~/utils/device'
 
 const useAction = <T,>(action: () => Promise<T>): [() => void, Resource<T>] => {
   const [source, setSource] = createSignal(false)
@@ -401,12 +402,18 @@ const PrimeManage: VoidComponent<{ dongleId: string }> = (props) => {
 
 const SettingsActivity: VoidComponent<PrimeActivityProps> = (props) => {
   const [device] = createResource(() => props.dongleId, getDevice)
+  const [deviceName] = createResource(device, getDeviceName)
   return (
     <>
       <TopAppBar leading={<IconButton class="md:hidden" name="arrow_back" href={`/${props.dongleId}`} />}>Device Settings</TopAppBar>
-      <div class="max-w-lg px-4">
-        <h2 class="mb-4 text-headline-sm">comma prime</h2>
-        <Suspense>
+      <div class="flex flex-col gap-4 max-w-lg px-4">
+        <h2 class="text-headline-sm">{deviceName()}</h2>
+        <Button color="error" leading={<Icon name="delete" />}>
+          Unpair this device
+        </Button>
+        <hr class="mx-4 opacity-20" />
+        <h2 class="text-headline-sm">comma prime</h2>
+        <Suspense fallback={<div class="h-64 skeleton-loader rounded-md" />}>
           <Switch>
             <Match when={device()?.prime === false}>
               <PrimeCheckout dongleId={props.dongleId} />
