18019d1eb117805719a77c5eb076ccde3ba96c24
ugtthis <dratunuman@gmail.com>
2025-03-26 11:05:37 -0700

    check timeline() before calling

--- src/components/RouteStatistics.tsx
+++ src/components/RouteStatistics.tsx
@@ -21,7 +21,7 @@ const RouteStatistics: VoidComponent<{ class?: string; route: Route }> = (props)
       statistics={[
         { label: 'Distance', value: () => formatDistance(props.route?.length) },
         { label: 'Duration', value: () => formatRouteDuration(props.route) },
-        { label: 'Engaged', value: () => formatEngagement(timeline()) },
+        { label: 'Engaged', value: () => (timeline() ? formatEngagement(timeline()) : undefined) },
         { label: 'User flags', value: () => timeline()?.userFlags },
       ]}
     />
