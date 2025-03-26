commaai/new-connect

commit fe511dc8d4ec7faaa71d02a966ede94e686f31ca

feat: upload user files (#216)
- setup API for athena and uploading files to comma
- add file upload buttons to route page

---------

Co-authored-by: Cameron Clough <cameronjclough@gmail.com>

src/api/athena.ts
@@ -1,0 +1,46 @@
+import {
+  AthenaCallResponse, BackendAthenaCallResponse, BackendAthenaCallResponseError, CancelUploadRequest,
+  CancelUploadResponse, UploadFile, UploadFilesToUrlsRequest, UploadFilesToUrlsResponse, UploadQueueItem,
+} from '~/types'
+import { fetcher } from '.'
+import { ATHENA_URL } from './config'
+
+// Higher number is lower priority
+const HIGH_PRIORITY = 0
+
+// Uploads expire after 1 week if device remains offline
+const EXPIRES_IN_SECONDS = 60 * 60 * 24 * 7
+
+export const cancelUpload = (dongleId: string, ids: string[]) =>
+  makeAthenaCall<CancelUploadRequest, CancelUploadResponse>(dongleId, 'cancelUpload', { upload_id: ids })
+
+export const getNetworkMetered = (dongleId: string) =>
+  makeAthenaCall<void, boolean>(dongleId, 'getNetworkMetered')
+
+export const getUploadQueue = (dongleId: string) =>
+  makeAthenaCall<void, UploadQueueItem[]>(dongleId, 'listUploadQueue')
+
+export const uploadFilesToUrls = (dongleId: string, files: UploadFile[]) =>
+  makeAthenaCall<UploadFilesToUrlsRequest, UploadFilesToUrlsResponse>(dongleId, 'uploadFilesToUrls', {
+    files_data: files.map((file) => ({
+      allow_cellular: false,
+      fn: file.filePath,
+      headers: file.headers,
+      priority: HIGH_PRIORITY,
+      url: file.url,
+    })),
+  }, Math.floor(Date.now() / 1000) + EXPIRES_IN_SECONDS)
+
+export const makeAthenaCall = async <REQ, RES>(dongleId: string, method: string, params?: REQ, expiry?: number): Promise<AthenaCallResponse<RES>> => {
+  const res = await fetcher<BackendAthenaCallResponse<RES> | BackendAthenaCallResponseError>(`/${dongleId}`, {
+    method: 'POST',
+    body: JSON.stringify({ id: 0, jsonrpc: '2.0', method, params, expiry }),
+  }, ATHENA_URL)
+  if ('error' in res) {
+    return { queued: false, error: res.error, result: undefined }
+  }
+  if (typeof res.result === 'string' && res.result === 'Device offline, message queued') {
+    return { queued: true, error: undefined, result: undefined }
+  }
+  return { queued: false, error: undefined, result: res.result as RES }
+}

src/api/file.ts
@@ -1,0 +1,11 @@
+import { Files, Route, UploadFileMetadataResponse } from '~/types'
+import { fetcher } from '.'
+
+export const getAlreadyUploadedFiles = (routeName: Route['fullname']): Promise<Files> =>
+  fetcher<Files>(`/v1/route/${routeName}/files`)
+
+export const requestToUploadFiles = (dongleId: string, paths: string[], expiryDays: number = 7) =>
+  fetcher<UploadFileMetadataResponse>(`/v1/${dongleId}/upload_urls/`, {
+    method: 'POST',
+    body: JSON.stringify({ expiry_days: expiryDays, paths }),
+  })

src/api/index.ts
@@ -12,6 +12,7 @@ export async function fetcher<T>(endpoint: string, init?: RequestInit, apiUrl: s
   const res = await fetch(`${apiUrl}${endpoint}`, {
     ...init,
     headers: {
+      'Content-Type': 'application/json',
       ...init?.headers,
       Authorization: `JWT ${getAccessToken()}`,
     },

src/api/devices.ts
@@ -1,5 +1,4 @@
-import type { Device, DeviceLocation, DrivingStatistics } from '~/types'
-
+import type { AthenaOfflineQueueResponse, Device, DeviceLocation, DrivingStatistics } from '~/types'
 import { fetcher } from '.'
 
 const sortDevices = (devices: Device[]) => devices.sort((a, b) => {
@@ -14,6 +13,9 @@ const sortDevices = (devices: Device[]) => devices.sort((a, b) => {
   }
 })
 
+export const getAthenaOfflineQueue = async (dongleId: string) =>
+  fetcher<AthenaOfflineQueueResponse>(`/v1/devices/${dongleId}/athena_offline_queue`)
+
 export const getDevice = async (dongleId: string) =>
   fetcher<Device>(`/v1.1/devices/${dongleId}/`)
 
@@ -24,9 +26,7 @@ export const getDeviceStats = async (dongleId: string) =>
   fetcher<DrivingStatistics>(`/v1.1/devices/${dongleId}/stats`)
 
 export const getDevices = async () =>
-  fetcher<Device[]>('/v1/me/devices/')
-    .then(sortDevices)
-    .catch(() => [])
+  fetcher<Device[]>('/v1/me/devices/').then(sortDevices).catch(() => [])
 
 const validatePairToken = (input: string): {
   identity: string

src/api/route.ts
@@ -1,24 +1,33 @@
-import type { Route, RouteShareSignature } from '~/types'
+import type { Route, RouteInfo, RouteShareSignature, RouteWithSegments } from '~/types'
 
 import { fetcher } from '.'
 import { BASE_URL } from './config'
 
+export const parseRouteName = (routeName: string): RouteInfo => {
+  const [dongleId, routeId] = routeName.split('|')
+  return { dongleId, routeId }
+}
+
 export const getRoute = (routeName: Route['fullname']): Promise<Route> =>
   fetcher<Route>(`/v1/route/${routeName}/`)
+
+export const getRouteWithSegments = async (routeName: Route['fullname']) => {
+  const { dongleId } = parseRouteName(routeName)
+  const routes = await fetcher<RouteWithSegments[]>(`/v1/devices/${dongleId}/routes_segments?${new URLSearchParams({ route_str: routeName }).toString()}`)
+  if (routes.length === 0) {
+    throw new Error('route does not exist')
+  }
+  return routes[0]
+}
 
 export const getRouteShareSignature = (routeName: string): Promise<RouteShareSignature> =>
   fetcher(`/v1/route/${routeName}/share_signature`)
 
-export const createQCameraStreamUrl = (
-  routeName: Route['fullname'],
-  signature: RouteShareSignature,
-): string =>
+export const createQCameraStreamUrl = (routeName: Route['fullname'], signature: RouteShareSignature): string =>
   `${BASE_URL}/v1/route/${routeName}/qcamera.m3u8?${new URLSearchParams(signature).toString()}`
 
 export const getQCameraStreamUrl = (routeName: Route['fullname']): Promise<string> =>
-  getRouteShareSignature(routeName).then((signature) =>
-    createQCameraStreamUrl(routeName, signature),
-  )
+  getRouteShareSignature(routeName).then((signature) => createQCameraStreamUrl(routeName, signature))
 
 export const setRoutePublic = (routeName: string, isPublic: boolean): Promise<Route> =>
   fetcher<Route>(`/v1/route/${routeName}/`, {
@@ -33,6 +42,4 @@ export const getPreservedRoutes = (dongleId: string): Promise<Route[]> =>
   fetcher<Route[]>(`/v1/devices/${dongleId}/routes/preserved`)
 
 export const setRoutePreserved = (routeName: string, preserved: boolean): Promise<Route> =>
-  fetcher<Route>(`/v1/route/${routeName}/preserve`, {
-    method: preserved ? 'POST' : 'DELETE',
-  })
+  fetcher<Route>(`/v1/route/${routeName}/preserve`, { method: preserved ? 'POST' : 'DELETE' })

src/api/upload.ts
@@ -1,0 +1,48 @@
+import { RouteInfo, UploadFile, UploadFileMetadata } from '~/types'
+import { uploadFilesToUrls } from './athena'
+import { getAlreadyUploadedFiles, requestToUploadFiles } from './file'
+import { parseRouteName } from './route'
+
+export const FileTypes = {
+  logs: ['rlog.bz2', 'rlog.zst'],
+  cameras: ['fcamera.hevc'],
+  dcameras: ['dcamera.hevc'],
+  ecameras: ['ecamera.hevc'],
+}
+
+type FileType = keyof typeof FileTypes
+
+const getFiles = async (routeName: string, types?: FileType[]) => {
+  const files = await getAlreadyUploadedFiles(routeName)
+  if (!types) return [...files.cameras, ...files.dcameras, ...files.ecameras, ...files.logs]
+  return types.flatMap(type => files[type])
+}
+
+const generateMissingFilePaths = (routeInfo: RouteInfo, segmentStart: number, segmentEnd: number, uploadedFiles: string[], types?: (keyof typeof FileTypes)[]): string[] => {
+  const paths: string[] = []
+  for (let i = segmentStart; i <= segmentEnd; i++) {
+    const fileTypes = types ? types.flatMap(type => FileTypes[type]) : Object.values(FileTypes).flat()
+    for (const fileName of fileTypes) {
+      const key = [routeInfo.dongleId, routeInfo.routeId, i, fileName].join('/')
+      if (!uploadedFiles.find((path) => path.includes(key))) {
+        paths.push(`${routeInfo.routeId}--${i}/${fileName}`)
+      }
+    }
+  }
+  return paths
+}
+
+const prepareUploadRequests = (paths: string[], presignedUrls: UploadFileMetadata[]): UploadFile[] =>
+  paths.map((path, i) => ({ filePath: path, ...presignedUrls[i] }))
+
+export const uploadAllSegments = (routeName: string, totalSegments: number, types?: (keyof typeof FileTypes)[]) => 
+  uploadSegments(routeName, 0, totalSegments - 1, types);
+
+export const uploadSegments = async (routeName: string, segmentStart: number, segmentEnd: number, types?: (keyof typeof FileTypes)[]) => {
+  const routeInfo = parseRouteName(routeName)
+  const alreadyUploadedFiles = await getFiles(routeName, types)
+  const paths = generateMissingFilePaths(routeInfo, segmentStart, segmentEnd, alreadyUploadedFiles, types)
+  const pathPresignedUrls = await requestToUploadFiles(routeInfo.dongleId, paths)
+  const athenaRequests = prepareUploadRequests(paths, pathPresignedUrls)
+  return await uploadFilesToUrls(routeInfo.dongleId, athenaRequests)
+}

src/ci/check_bundle_size.ts
@@ -35,7 +35,7 @@ files.push(
 console.table(files, ['path', 'sizeKB', 'compressedSizeKB'])
 
 const lowerBoundKB = 200;
-const upperBoundKB = 245;
+const upperBoundKB = 250;
 if (totalCompressedSize < lowerBoundKB * 1024) {
   console.warn(`Bundle size lower than expected, let's lower the limit! (${totalCompressedSizeKB}KB < ${lowerBoundKB}KB)`)
   process.exit(1)

src/components/RouteActions.tsx
@@ -1,12 +1,8 @@
-import { createSignal, Show, type VoidComponent, createEffect, createResource } from 'solid-js'
-import clsx from 'clsx'
+import { createSignal, Show, type VoidComponent, createEffect, createResource } from "solid-js"
+import clsx from "clsx"
 
-import { setRoutePublic, setRoutePreserved, getRoute, getPreservedRoutes } from '~/api/route'
-import Icon from '~/components/material/Icon'
-
-interface RouteActionsProps {
-  routeName: string
-}
+import { setRoutePublic, setRoutePreserved, getPreservedRoutes, parseRouteName, getRoute } from "~/api/route"
+import Icon from "~/components/material/Icon"
 
 const ToggleButton: VoidComponent<{
   label: string
@@ -34,10 +30,14 @@ const ToggleButton: VoidComponent<{
   </button>
 )
 
+interface RouteActionsProps {
+  routeName: string
+}
+
 const RouteActions: VoidComponent<RouteActionsProps> = (props) => {
   const [routeResource] = createResource(() => props.routeName, getRoute)
   const [preservedRoutesResource] = createResource(
-    () => props.routeName.split('|')[0],
+    () => parseRouteName(props.routeName).dongleId,
     getPreservedRoutes,
   )
 

commit cde66b375ab752ca35b3867f94083a3f309d14df

fix(api): fix pilotpair request Content-Type (#248)
bug introduced in fe511dc8d4ec7faaa71d02a966ede94e686f31ca

src/api/athena.ts
@@ -55,6 +55,7 @@ export const makeAthenaCall = async <REQ, RES>(
     {
       method: 'POST',
       body: JSON.stringify({ id: 0, jsonrpc: '2.0', method, params, expiry }),
+      headers: { 'Content-Type': 'application/json' },
     },
     ATHENA_URL,
   )

src/api/file.ts
@@ -7,4 +7,5 @@ export const requestToUploadFiles = (dongleId: string, paths: string[], expiryDa
   fetcher<UploadFileMetadataResponse>(`/v1/${dongleId}/upload_urls/`, {
     method: 'POST',
     body: JSON.stringify({ expiry_days: expiryDays, paths }),
+    headers: { 'Content-Type': 'application/json' },
   })

src/api/index.ts
@@ -12,7 +12,6 @@ export async function fetcher<T>(endpoint: string, init?: RequestInit, apiUrl: s
   const res = await fetch(`${apiUrl}${endpoint}`, {
     ...init,
     headers: {
-      'Content-Type': 'application/json',
       ...init?.headers,
       Authorization: `JWT ${getAccessToken()}`,
     },