4162d88c3641707fe8aa89291ce81e1da9856c75
Trey Moen <50057480+greatgitsby@users.noreply.github.com>
2025-03-27 08:43:16 -0700

    feat: upload queue (#251)
    last part of https://github.com/commaai/connect/issues/51
    
    handles device disconnected, no items in queue, and rendering a list of
    files being uploaded
    
    possible features to add in the future:
    - nice animations
    - displaying the offline queue
    - simplify the polling loop with something like
    https://tanstack.com/query/latest/docs/framework/solid/overview

---
+++ src/components/UploadQueue.tsx
@@ -0,0 +1,108 @@
+import { createSignal, For, Match, onCleanup, Switch, VoidComponent } from 'solid-js'
+import { COMMA_CONNECT_PRIORITY, getUploadQueue } from '~/api/athena'
+import { UploadQueueItem } from '~/types'
+import LinearProgress from './material/LinearProgress'
+import Icon from './material/Icon'
+import { createStore, reconcile } from 'solid-js/store'
+import clsx from 'clsx'
+
+interface DecoratedUploadQueueItem extends UploadQueueItem {
+  route: string
+  segment: number
+  filename: string
+}
+
+const parseUploadPath = (url: string) => {
+  const parts = new URL(url).pathname.split('/')
+  return { route: parts[3], segment: parseInt(parts[4], 10), filename: parts[5] }
+}
+
+const UploadQueueRow: VoidComponent<{ item: DecoratedUploadQueueItem }> = ({ item }) => {
+  return (
+    <div class="flex flex-col">
+      <div class="flex items-center justify-between flex-wrap mb-1 gap-x-4 min-w-0">
+        <div class="flex items-center min-w-0 flex-1">
+          <Icon
+            class="text-on-surface-variant flex-shrink-0 mr-2"
+            name={item.priority === COMMA_CONNECT_PRIORITY ? 'person' : 'local_fire_department'}
+          />
+          <div class="flex min-w-0 gap-1">
+            <span class="text-body-sm font-mono truncate text-on-surface">{[item.route, item.segment, item.filename].join(' ')}</span>
+          </div>
+        </div>
+        <div class="flex items-center gap-2 flex-shrink-0 justify-end">
+          <span class="text-body-sm font-mono whitespace-nowrap">{Math.round(item.progress * 100)}%</span>
+        </div>
+      </div>
+      <div class="h-1.5 w-full overflow-hidden rounded-full bg-surface-container-highest">
+        <LinearProgress progress={item.progress} color={Math.round(item.progress * 100) === 100 ? 'tertiary' : 'primary'} />
+      </div>
+    </div>
+  )
+}
+
+const WAITING = 'Waiting for device to connect...'
+
+const UploadQueue: VoidComponent<{ dongleId: string }> = (props) => {
+  const [error, setError] = createSignal<string | undefined>(WAITING)
+  const [items, setItems] = createStore<DecoratedUploadQueueItem[]>([])
+
+  let timeout: Timer | undefined
+
+  const fetch = () => {
+    getUploadQueue(props.dongleId)
+      .then((res) => {
+        if (res.error) {
+          setError(res.error)
+          return
+        }
+        setItems(
+          reconcile(res.result?.map((item) => ({ ...item, ...parseUploadPath(item.url) })).sort((a, b) => b.progress - a.progress) || []),
+        )
+        setError(undefined)
+      })
+      .catch((error) => {
+        if (error instanceof Error && error.cause instanceof Response && error.cause.status === 404) {
+          setError(WAITING)
+          return
+        }
+        setError(error.toString())
+      })
+      .finally(() => {
+        if (!timeout) return
+        timeout = setTimeout(fetch, 1000)
+      })
+  }
+
+  timeout = setTimeout(fetch, 0)
+
+  onCleanup(() => {
+    clearTimeout(timeout)
+    timeout = undefined
+  })
+
+  return (
+    <div class="flex flex-col p-4 gap-4 bg-surface-container-lowest">
+      <div class="relative h-[calc(4*3rem)] sm:h-[calc(6*3rem)] flex justify-center items-center text-on-surface-variant">
+        <Switch
+          fallback={
+            <div class="absolute inset-0 flex flex-col gap-2 overflow-y-auto hide-scrollbar">
+              <For each={items}>{(item) => <UploadQueueRow item={item} />}</For>
+            </div>
+          }
+        >
+          <Match when={error()}>
+            <Icon class={clsx(error() === WAITING && 'animate-spin')} name={error() === WAITING ? 'progress_activity' : 'error'} />
+            <span class="ml-2">{error()}</span>
+          </Match>
+          <Match when={items.length === 0}>
+            <Icon name="check" />
+            <span class="ml-2">Nothing to upload</span>
+          </Match>
+        </Switch>
+      </div>
+    </div>
+  )
+}
+
+export default UploadQueue
