8126781b68380eb69cab97b0f4c6aa3385763d06
Cameron Clough <cameronjclough@gmail.com>
2025-03-27 17:10:44 +0000

    update

--- src/pages/offline.tsx
+++ src/pages/offline.tsx
@@ -3,17 +3,18 @@ import Icon from '~/components/material/Icon'
 
 export default function Offline() {
   return (
-    <div class="flex min-h-screen flex-col gap-8 items-center justify-center bg-background p-6">
-      <div class="flex max-w-sm flex-col items-center gap-4 text-body-lg">
+    <div class="flex min-h-screen flex-col gap-12 items-center justify-center bg-background p-6">
+      <div class="flex max-w-sm flex-col items-center gap-4">
         <img src="/images/logo-connect-light.svg" alt="comma connect" width={96} height={96} />
-        <div class="flex flex-col gap-8 items-center ">
+        <div class="flex flex-col gap-2 items-center">
           <h1 class="text-display-sm">comma connect</h1>
-          <div class="flex items-center gap-2">
+          <div class="flex items-center gap-3">
             <span class="size-2 rounded-full bg-error-container" />
-            <p>offline</p>
+            <p class="text-title-lg">offline</p>
           </div>
         </div>
       </div>
+      <p class="text-body-lg">Please check your network connection</p>
       <Button color="secondary" trailing={<Icon name="refresh" />} onClick={() => window.location.reload()}>
         Try again
       </Button>
