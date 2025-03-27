d12c93d48fb15ae533c1448a0803ba41683cd359
Cynamide <arjitagarwal123@gmail.com>
2025-03-27 17:21:14 -0400

    fixed route statistics

--- src/components/RouteStatistics.tsx
+++ src/components/RouteStatistics.tsx
@@ -1,30 +1,30 @@
-import { createResource } from 'solid-js'
-import type { VoidComponent } from 'solid-js'
+import { createResource } from "solid-js";
+import type { VoidComponent } from "solid-js";
 
-import { TimelineStatistics, getTimelineStatistics } from '~/api/derived'
-import type { Route } from '~/types'
-import { formatDistance, formatRouteDuration } from '~/utils/format'
-import StatisticBar from './StatisticBar'
+import { TimelineStatistics, getTimelineStatistics } from "~/api/derived";
+import type { Route } from "~/types";
+import { formatDistance, formatRouteDuration } from "~/utils/format";
+import StatisticBar from "./StatisticBar";
 
 const formatEngagement = (timeline?: TimelineStatistics): string | undefined => {
-  if (!timeline) return undefined
-  const { engagedDuration, duration } = timeline
-  return `${(100 * (engagedDuration / duration)).toFixed(0)}%`
-}
+  if (!timeline) return undefined;
+  const { engagedDuration, duration } = timeline;
+  return `${(100 * (engagedDuration / duration)).toFixed(0)}%`;
+};
 
 const RouteStatistics: VoidComponent<{ class?: string; route?: Route }> = (props) => {
-  const [timeline] = createResource(() => props.route, getTimelineStatistics)
+  const [timeline] = createResource(() => props.route, getTimelineStatistics);
 
   return (
     <StatisticBar
       class={props.class}
       statistics={[
-        { label: 'Distance', value: () => formatDistance(props.route?.length) },
-        { label: 'Duration', value: () => (props.route ? formatRouteDuration(props.route) : undefined) },
-        { label: 'Engaged', value: () => formatEngagement(timeline()) },
+        { label: "Distance", value: () => formatDistance(props.route?.length) },
+        { label: "Duration", value: () => (props.route ? formatRouteDuration(props.route) : undefined) },
+        { label: "Engaged", value: () => formatEngagement(timeline()) },
       ]}
     />
-  )
-}
+  );
+};
 
-export default RouteStatistics
+export default RouteStatistics;
