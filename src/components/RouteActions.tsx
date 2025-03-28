4fdbb25ff2bdb2fa098a6d9abef29ad0f6f848c6
Trey Moen <trey@moen.ai>
2025-03-28 13:51:03 -0700

    Revert "inline"
    This reverts commit 843772f8a393db6a97d6bd02b449a156e0c11241.

--- src/components/RouteActions.tsx
+++ src/components/RouteActions.tsx
@@ -41,6 +41,8 @@ const RouteActions: VoidComponent<RouteActionsProps> = (props) => {
   const [isPublic, setIsPublic] = createSignal<boolean | undefined>(undefined)
   const [isPreserved, setIsPreserved] = createSignal<boolean | undefined>(undefined)
 
+  const useradminUrl = () => `https://useradmin.comma.ai/?onebox=${currentRouteId()}`
+
   createEffect(() => {
     const route = routeResource()
     const preservedRoutes = preservedRoutesResource()
@@ -105,12 +107,7 @@ const RouteActions: VoidComponent<RouteActionsProps> = (props) => {
       <div class="font-mono text-body-sm text-zinc-500">
         <div class="flex justify-between">
           <h3 class="mb-2 text-on-surface-variant">Route ID:</h3>
-          <a
-            href={`https://useradmin.comma.ai/?onebox=${currentRouteId()}`}
-            class="text-blue-400 hover:text-blue-500 duration-200"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a href={useradminUrl()} class="text-blue-400 hover:text-blue-500 duration-200" target="_blank" rel="noopener noreferrer">
             View in useradmin
           </a>
         </div>
