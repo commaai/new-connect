d9ec614e059418b2e5d2631e57d667156840e0f9
Shane Smiskol <shane@smiskol.com>
2025-03-29 01:45:44 -0700

    lgtm

--- src/pages/dashboard/components/RouteList.tsx
+++ src/pages/dashboard/components/RouteList.tsx
@@ -72,7 +72,7 @@ type RouteListProps = {
 
 const RouteList: VoidComponent<RouteListProps> = (props) => {
   const dimensions = useDimensions()
-  const pageSize = () => 6;  // Math.max(Math.ceil(dimensions().height / 2 / 140), 1)
+  const pageSize = () => Math.max(Math.ceil(dimensions().height / 2 / 140), 1)
   const endpoint = () => `/v1/devices/${props.dongleId}/routes_segments?limit=${pageSize()}`
   const getKey = (previousPageData?: RouteSegments[]): string | undefined => {
     if (!previousPageData) return endpoint()
