d88c5e2ae5ce997ddb2fdb566d9d1cee29d82a63
Trey Moen <trey@moen.ai>
2025-03-29 14:41:03 -0700

    fix: typo

--- src/components/UploadQueue.tsx
+++ src/components/UploadQueue.tsx
@@ -22,7 +22,7 @@ const parseUploadPath = (url: string) => {
   if (parsed.hostname === 'upload.commadotai.com') {
     return { route: parts[2], segment: parseInt(parts[3], 10), filename: parts[4], isFirehose: true }
   }
-  return { route: parts[3], segment: parseInt(parts[4], 10), filename: parts[5], isFirehouse: false }
+  return { route: parts[3], segment: parseInt(parts[4], 10), filename: parts[5], isFirehose: false }
 }
 
 const cancel = (dongleId: string, ids: string[]) => {
