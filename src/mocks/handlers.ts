a993865a3654fdc9008d107547ee28720e49064d
Cameron Clough <cameronjclough@gmail.com>
2025-03-30 23:32:07 +0100

    clean

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -3,14 +3,16 @@ import { http, HttpResponse } from 'msw'
 import { API_URL } from '~/api/config'
 import type { Profile } from '~/types'
 
+const api = (path: string) => new URL(path, API_URL).toString()
+
 export const handlers = [
-  http.get(`${API_URL}/v1/me`, () => {
-    return HttpResponse.json<Profile>({
+  http.get(api('/v1/me'), () =>
+    HttpResponse.json<Profile>({
       id: '0123456789abcdef',
       email: 'user@comma.ai',
       regdate: 1735689600,
       superuser: true,
       user_id: 'google_0123456789abcdef',
-    })
-  }),
+    }),
+  ),
 ]
