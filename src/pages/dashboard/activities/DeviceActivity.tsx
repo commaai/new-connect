cd026a225cf94c1f0f08c3b1c30a08aa058d7f06
Trey Moen <trey@moen.ai>
2025-03-29 20:39:17 -0700

    this was test

--- src/pages/dashboard/activities/DeviceActivity.tsx
+++ src/pages/dashboard/activities/DeviceActivity.tsx
@@ -121,9 +121,7 @@ const DeviceActivity: VoidComponent<DeviceActivityProps> = (props) => {
       <div class="flex flex-col gap-4 px-4 pb-4">
         <div class="h-min overflow-hidden rounded-lg bg-surface-container-low">
           <Show when={deviceName()}>
-            <Suspense fallback={<div class="skeleton-loader size-full" />}>
-              <DeviceLocation dongleId={props.dongleId} deviceName={deviceName()!} />
-            </Suspense>
+            <DeviceLocation dongleId={props.dongleId} deviceName={deviceName()!} />
           </Show>
           <div class="flex items-center justify-between p-4">
             <div class="text-xl font-bold">{deviceName()}</div>
@@ -179,9 +177,7 @@ const DeviceActivity: VoidComponent<DeviceActivityProps> = (props) => {
             </div>
           )}
         </div>
-        <Suspense fallback={<div class="skeleton-loader size-full" />}>
-          <RouteList dongleId={props.dongleId} />
-        </Suspense>
+        <RouteList dongleId={props.dongleId} />
       </div>
     </>
   )
