2d06de808626ef2e03350d9f8502342185117feb
Trey Moen <trey@moen.ai>
2025-03-31 08:23:49 -0700

    uploading messages bubble up first

--- src/queries/upload-queue.tsx
+++ src/queries/upload-queue.tsx
@@ -5,7 +5,11 @@ import { parseUploadPath } from '~/utils/parse'
 import { AthenaCallResponse, AthenaOfflineQueueResponse, UploadFilesToUrlsRequest, UploadQueueItem } from '~/types'
 
 const mapOnlineQueueItems = (data: AthenaCallResponse<UploadQueueItem[]>) =>
-  data.result?.map((item) => ({ ...item, ...parseUploadPath(item.url) })) || []
+  !data.result
+    ? []
+    : data.result
+        .map((item) => ({ ...item, ...parseUploadPath(item.url) }))
+        .sort((a, b) => (a.current === b.current ? 0 : a.current ? -1 : 1))
 
 const mapOfflineQueueItems = (data: AthenaOfflineQueueResponse) =>
   data
