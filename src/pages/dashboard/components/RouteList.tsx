c43658fa65b6cc89b0c98c123c0c20c5ade981a2
Shane Smiskol <shane@smiskol.com>
2025-03-30 20:32:38 -0700

    Revert "stash 2"
    This reverts commit 837b26a776374055a03dffa0554e6de5e2892a04.

--- src/pages/dashboard/components/RouteList.tsx
+++ src/pages/dashboard/components/RouteList.tsx
@@ -72,7 +72,7 @@ const RouteCard: VoidComponent<RouteCardProps> = (props) => {
   )
 }
 
-const PAGE_SIZE = 2
+const PAGE_SIZE = 10
 
 const RouteList: VoidComponent<{ dongleId: string }> = (props) => {
   const endpoint = () => `/v1/devices/${props.dongleId}/routes_segments?limit=${PAGE_SIZE}`
@@ -82,71 +82,24 @@ const RouteList: VoidComponent<{ dongleId: string }> = (props) => {
     return `${endpoint()}&end=${previousPageData.at(-1)!.start_time_utc_millis - 1}`
   }
   const getPage = (page: number): Promise<RouteSegments[]> => {
-    console.log('getPage', page)
-    // const day = dayjs(route.start_time_utc_millis).format('ddd, MMM D, YYYY')
-    // const day = 'Day 1 2025'  // TODO remove this
-    // if (!days.has(day)) {
-    //   days.set(day, [])
-    // }
-    if (!daysNew.has(page)) {
-      daysNew.set(page, [])
-      console.log("adding empty day list", page)
+    if (pages[page] === undefined) {
+      pages[page] = (async () => {
+        const previousPageData = page > 0 ? await getPage(page - 1) : undefined
+        const key = getKey(previousPageData)
+        return key ? fetcher<RouteSegments[]>(key) : []
+      })()
     }
-
-    const promise = (async () => {
-      const previousPageData = page > 0 ? await getPage(page - 1) : undefined
-      const key = getKey(previousPageData)
-      return key ? fetcher<RouteSegments[]>(key) : []
-    })();
-
-    promise.then((data) => {  // rename routes
-      console.log("got data", data)
-      for (const route of data) {
-        const day = dayjs(route.start_time_utc_millis).format('ddd, MMM D, YYYY')
-        if (!dayIdxMap.has(day)) {
-          dayIdxMap.set(day, dayIdxMap.size)
-          console.log("adding dayIdxMap", dayIdxMap)
-        }
-        const dayIdx = dayIdxMap.get(day)!
-        // if (!daysNew.has(dayIdx)) {
-        //   console.log("adding day123", day)
-        //   daysNew.set(dayIdx, [])
-        // }
-        daysNew.get(dayIdx)!.push(route)
-        console.log('daysNew', daysNew)
-      }
-    })
-
-    // days.get(day)!.push(promise)
-
-    // if (pages[page] === undefined) {
-    //   pages[page] = (async () => {
-    //     const previousPageData = page > 0 ? await getPage(page - 1) : undefined
-    //     const key = getKey(previousPageData)
-    //     return key ? fetcher<RouteSegments[]>(key) : []
-    //   })()
-    // }
-    // return Promise.all([pages[page]]).then(results => results.flat());
-    // return Promise.all(days.get(day)).then(results => results.flat());
-    console.log("returning", daysNew.get(page))
-    return daysNew.get(page)
+    return pages[page]
   }
 
-  let dayIdxMap = new Map<string, number>()
-
-  const days = new Map<string, Promise<RouteSegments[]>[]>()
-  const daysNew = new Map<number, Promise<RouteSegments[]>[]>()
-  // const daysNew: Promise<RouteSegments[]>[][] = [];
-
-  const pages: Promise<RouteSegments[]>[][] = [];
+  const pages: Promise<RouteSegments[]>[] = []
   const [size, setSize] = createSignal(1)
   const pageNumbers = () => Array.from({ length: size() })
 
   createEffect(() => {
     if (props.dongleId) {
       pages.length = 0
-      getPage(0)
-      // setSize(1)
+      setSize(1)
     }
   })
 
@@ -155,7 +108,6 @@ const RouteList: VoidComponent<{ dongleId: string }> = (props) => {
     (entries) => {
       if (entries[0].isIntersecting) {
         setSize((prev) => prev + 1)
-        getPage(0)
       }
     },
     { threshold: 0.1 },
@@ -170,8 +122,6 @@ const RouteList: VoidComponent<{ dongleId: string }> = (props) => {
 
   return (
     <div class="flex w-full flex-col justify-items-stretch gap-4">
-      {/*<For each={groupRoutes([])}>*/}
-
       {/*TODO: this results in duplicate headers*/}
       <For each={pageNumbers()}>
         {(_, i) => {
