11d01a8610b83ec608661bfb8d54b9b44c57abec
Cameron Clough <cameronjclough@gmail.com>
2025-03-31 20:46:59 +0100

    final padding

--- pwa-assets.config.ts
+++ pwa-assets.config.ts
@@ -8,15 +8,16 @@ export default defineConfig({
     ...minimal2023Preset,
     maskable: {
       ...minimal2023Preset.maskable,
-      padding: 0.4,
+      padding: 0.5,
       resizeOptions: { background: '#131318' },
     },
     apple: {
       ...minimal2023Preset.apple,
+      padding: 0.5,
       resizeOptions: { background: '#131318' },
     },
     appleSplashScreens: createAppleSplashScreens({
-      padding: 0.5,
+      padding: 0.7,
       resizeOptions: { fit: 'contain', background: '#131318' },
       linkMediaOptions: {
         log: true,
