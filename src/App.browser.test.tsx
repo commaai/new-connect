6154975a25732a0ec14a66e67a73d6b6080a3db2
Cameron Clough <cameronjclough@gmail.com>
2025-03-31 16:04:27 +0100

    update tests

--- src/App.browser.test.tsx
+++ src/App.browser.test.tsx
@@ -6,28 +6,24 @@ import * as Demo from '~/api/auth/demo'
 import { AppLayout, Routes } from './App'
 
 const DEMO_LOG_ID = '000000dd--455f14369d'
+const UPLOAD_QUEUE = 'Upload Queue'
 
 const renderApp = (location: string) => render(() => <Routes />, { location, wrapper: AppLayout })
 
 beforeAll(() => configure({ asyncUtilTimeout: 2000 }))
-beforeEach(() => clearAccessToken())
-
-test('Show login page', async () => {
-  const { findByText } = renderApp('/')
-  expect(await findByText('Sign in with Google')).not.toBeFalsy()
-})
 
 describe('Demo mode', () => {
   beforeEach(() => setAccessToken(Demo.ACCESS_TOKEN))
 
   test('View dashboard', async () => {
     const { findByText } = renderApp('/')
-    expect(await findByText('demo 3X')).not.toBeFalsy()
+    expect(await findByText('demo 3X')).toBeTruthy()
+    expect(await findByText(UPLOAD_QUEUE)).toBeTruthy()
   })
 
   test('View demo route', async () => {
     const { findByText, findByTestId } = renderApp(`/${Demo.DONGLE_ID}/${DEMO_LOG_ID}`)
-    expect(await findByText(DEMO_LOG_ID)).not.toBeFalsy()
+    expect(await findByText(DEMO_LOG_ID)).toBeTruthy()
     const video = (await findByTestId('route-video')) as HTMLVideoElement
     await waitFor(() => expect(video.src).toBeTruthy())
   })
@@ -36,6 +32,13 @@ describe('Demo mode', () => {
 // TODO: write tests/setup second demo for read-only access tests
 
 describe('Public routes', () => {
+  beforeEach(() => clearAccessToken())
+
+  test('Show login page', async () => {
+    const { findByText } = renderApp('/')
+    expect(await findByText('Sign in with Google')).toBeTruthy()
+  })
+
   test('Require route ID in URL', async () => {
     const { findByText } = renderApp(`/${Demo.DONGLE_ID}`)
     expect(await findByText('Sign in with Google')).toBeTruthy()
@@ -53,5 +56,12 @@ describe('Public routes', () => {
     setAccessToken(Demo.ACCESS_TOKEN)
     const { findByText } = renderApp(`/e886087f430e7fe7/00000221--604653e929`)
     expect(await findByText('00000221--604653e929')).toBeTruthy()
+  })
+
+  test('Viewing public route should not show device details', async () => {
+    const { findByTestId, queryByText } = renderApp(`/${Demo.DONGLE_ID}/${DEMO_LOG_ID}`)
+    const video = (await findByTestId('route-video')) as HTMLVideoElement
+    await waitFor(() => expect(video.src).toBeTruthy())
+    expect(queryByText(UPLOAD_QUEUE)).toBeFalsy()
   })
 })
