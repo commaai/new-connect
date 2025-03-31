0cc3ca68d04cced456fc53fa3623b4ddab415923
Shane Smiskol <shane@smiskol.com>
2025-03-30 21:38:40 -0700

    Revert "add today"
    This reverts commit b15566e1c4bf0363608c7f7cc094e07692be7764.

--- src/pages/dashboard/components/RouteList.tsx
+++ src/pages/dashboard/components/RouteList.tsx
@@ -106,19 +106,10 @@ const RouteList: VoidComponent<{ dongleId: string }> = (props) => {
   })
   onCleanup(() => observer.disconnect())
 
-  // Group and display headers for each day
   let prevDayHeader: string | null = null
   function getDayHeader(route: RouteSegments): string | null {
     const date = dayjs(route.start_time_utc_millis)
-    let dayHeader = null
-    if (date.isSame(dayjs(), 'day')) {
-      dayHeader = 'Today'
-    } else if (date.year() === dayjs().year()) {
-      dayHeader = date.format('dddd, MMM D')
-    } else {
-      dayHeader = date.format('dddd, MMM D, YYYY')
-    }
-
+    const dayHeader = date.format(date.year() === dayjs().year() ? 'dddd, MMM D' : 'dddd, MMM D, YYYY')
     if (dayHeader !== prevDayHeader) {
       prevDayHeader = dayHeader
       return dayHeader
