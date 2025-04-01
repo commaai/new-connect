82afffe85a108b708333461b11e287a1ae089564
Trey Moen <trey@moen.ai>
2025-03-31 22:12:47 -0700

    fix: snappier route load

--- src/pages/dashboard/activities/RouteActivity.tsx
+++ src/pages/dashboard/activities/RouteActivity.tsx
@@ -1,5 +1,6 @@
-import { createResource, createSignal, Match, Suspense, Switch, type VoidComponent } from 'solid-js'
+import { createEffect, createResource, createSignal, Match, Suspense, Switch, type VoidComponent } from 'solid-js'
 import { Navigate } from '@solidjs/router'
+import { createQuery } from '@tanstack/solid-query'
 
 import { setRouteViewed } from '~/api/athena'
 import { getDevice } from '~/api/devices'
@@ -27,8 +28,12 @@ const RouteActivity: VoidComponent<RouteActivityProps> = (props) => {
   const [videoRef, setVideoRef] = createSignal<HTMLVideoElement>()
 
   const routeName = () => `${props.dongleId}|${props.dateStr}`
-  const [route] = createResource(routeName, getRoute)
-  const [startTime] = createResource(route, (route) => dayjs(route.start_time)?.format('ddd, MMM D, YYYY'))
+  const route = createQuery(() => ({
+    queryKey: ['route', routeName()],
+    queryFn: () => getRoute(routeName()),
+    retry: false,
+  }))
+  const [startTime] = createResource(route, (route) => dayjs(route.data?.start_time)?.format('ddd, MMM D, YYYY'))
 
   const onTimelineChange = (newTime: number) => {
     const video = videoRef()
@@ -37,36 +42,33 @@ const RouteActivity: VoidComponent<RouteActivityProps> = (props) => {
 
   const [device] = createResource(() => props.dongleId, getDevice)
   const [profile] = createResource(getProfile)
-  createResource(
-    () => [device(), profile(), props.dateStr] as const,
-    async ([device, profile, dateStr]) => {
-      if (!device || !profile || (!device.is_owner && !profile.superuser)) return
-      await setRouteViewed(device.dongle_id, dateStr)
-    },
-  )
+  createEffect(() => {
+    if (!device() || !profile() || (!device()?.is_owner && !profile()?.superuser)) return
+    setRouteViewed(device()!.dongle_id, props.dateStr).catch(console.error)
+  })
 
   return (
     <>
       <TopAppBar leading={<IconButton class="md:hidden" name="arrow_back" href={`/${props.dongleId}`} />}>{startTime()}</TopAppBar>
 
       <Switch>
-        <Match when={route.error}>
+        <Match when={route.isError}>
           <Navigate href="/" />
         </Match>
-        <Match when={!route.loading}>
+        <Match when={route.isSuccess}>
           <div class="flex flex-col gap-6 px-4 pb-4">
             <div class="flex flex-col">
               <RouteVideoPlayer ref={setVideoRef} routeName={routeName()} startTime={seekTime()} onProgress={setSeekTime} />
-              <Timeline class="mb-1" route={route.latest} seekTime={seekTime()} updateTime={onTimelineChange} />
+              <Timeline class="mb-1" route={route.data} seekTime={seekTime()} updateTime={onTimelineChange} />
             </div>
 
             <div class="flex flex-col gap-2">
               <h3 class="text-label-sm uppercase">Route Info</h3>
               <div class="flex flex-col rounded-md overflow-hidden bg-surface-container">
-                <RouteStatistics class="p-5" route={route()} />
+                <RouteStatistics class="p-5" route={route.data} />
 
                 <Suspense fallback={<div class="skeleton-loader min-h-48" />}>
-                  <RouteActions routeName={routeName()} route={route()} />
+                  <RouteActions routeName={routeName()} route={route.data} />
                 </Suspense>
               </div>
             </div>
@@ -75,7 +77,7 @@ const RouteActivity: VoidComponent<RouteActivityProps> = (props) => {
               <h3 class="text-label-sm uppercase">Upload Files</h3>
               <div class="flex flex-col rounded-md overflow-hidden bg-surface-container">
                 <Suspense fallback={<div class="skeleton-loader min-h-48" />}>
-                  <RouteUploadButtons route={route()} />
+                  <RouteUploadButtons route={route.data} />
                 </Suspense>
               </div>
             </div>
@@ -84,7 +86,7 @@ const RouteActivity: VoidComponent<RouteActivityProps> = (props) => {
               <h3 class="text-label-sm uppercase">Route Map</h3>
               <div class="aspect-square overflow-hidden rounded-lg">
                 <Suspense fallback={<div class="skeleton-loader size-full bg-surface" />}>
-                  <RouteStaticMap route={route()} />
+                  <RouteStaticMap route={route.data} />
                 </Suspense>
               </div>
             </div>
