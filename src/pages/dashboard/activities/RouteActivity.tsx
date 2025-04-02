5f070c33d97766d06e89293bf45ee4d2ca330abc
Cameron Clough <cameronjclough@gmail.com>
2025-04-02 18:53:38 +0100

    maybe this

--- src/pages/dashboard/activities/RouteActivity.tsx
+++ src/pages/dashboard/activities/RouteActivity.tsx
@@ -1,8 +1,5 @@
 import { createResource, createSignal, Suspense, type VoidComponent } from 'solid-js'
 
-import { setRouteViewed } from '~/api/athena'
-import { getDevice } from '~/api/devices'
-import { getProfile } from '~/api/profile'
 import { getRoute } from '~/api/route'
 import { dayjs } from '~/utils/format'
 
@@ -10,11 +7,10 @@ import IconButton from '~/components/material/IconButton'
 import TopAppBar from '~/components/material/TopAppBar'
 import RouteActions from '~/components/RouteActions'
 import RouteStaticMap from '~/components/RouteStaticMap'
-import RouteStatistics from '~/components/RouteStatistics'
 import RouteVideoPlayer from '~/components/RouteVideoPlayer'
 import RouteUploadButtons from '~/components/RouteUploadButtons'
 import Timeline from '~/components/Timeline'
-import { generateTimelineStatistics, getTimelineEvents } from '~/api/derived'
+import { getTimelineEvents } from '~/api/derived'
 
 type RouteActivityProps = {
   dongleId: string
@@ -28,28 +24,31 @@ const RouteActivity: VoidComponent<RouteActivityProps> = (props) => {
 
   const routeName = () => `${props.dongleId}|${props.dateStr}`
   const [route] = createResource(routeName, getRoute)
-  const [startTime] = createResource(route, (route) => dayjs(route.start_time)?.format('ddd, MMM D, YYYY'))
+  const startTime = () => {
+    if (!route.latest) return ''
+    return dayjs(route.latest.start_time).format('ddd, MMM D, YYYY')
+  }
 
   const [events] = createResource(route, getTimelineEvents, { initialValue: [] })
-  const [timeline] = createResource(
-    () => [route.latest, events.latest] as const,
-    ([route, events]) => generateTimelineStatistics(route, events),
-  )
+  // const [timeline] = createResource(
+  //   () => [route.latest, events.latest] as const,
+  //   ([route, events]) => generateTimelineStatistics(route, events),
+  // )
 
   const onTimelineChange = (newTime: number) => {
     const video = videoRef()
     if (video) video.currentTime = newTime
   }
 
-  const [device] = createResource(() => props.dongleId, getDevice)
-  const [profile] = createResource(getProfile)
-  createResource(
-    () => [device(), profile(), props.dateStr] as const,
-    async ([device, profile, dateStr]) => {
-      if (!device || !profile || (!device.is_owner && !profile.superuser)) return
-      await setRouteViewed(device.dongle_id, dateStr)
-    },
-  )
+  // const [device] = createResource(() => props.dongleId, getDevice)
+  // const [profile] = createResource(getProfile)
+  // createResource(
+  //   () => [device(), profile(), props.dateStr] as const,
+  //   async ([device, profile, dateStr]) => {
+  //     if (!device || !profile || (!device.is_owner && !profile.superuser)) return
+  //     await setRouteViewed(device.dongle_id, dateStr)
+  //   },
+  // )
 
   return (
     <>
@@ -58,13 +57,13 @@ const RouteActivity: VoidComponent<RouteActivityProps> = (props) => {
       <div class="flex flex-col gap-6 px-4 pb-4">
         <div class="flex flex-col">
           <RouteVideoPlayer ref={setVideoRef} routeName={routeName()} startTime={seekTime()} onProgress={setSeekTime} />
-          <Timeline class="mb-1" route={route.latest} seekTime={seekTime()} updateTime={onTimelineChange} events={events()} />
+          <Timeline class="mb-1" route={route.latest} seekTime={seekTime()} updateTime={onTimelineChange} events={events.latest} />
         </div>
 
         <div class="flex flex-col gap-2">
           <h3 class="text-label-sm uppercase">Route Info</h3>
           <div class="flex flex-col rounded-md overflow-hidden bg-surface-container">
-            <RouteStatistics class="p-5" route={route()} timeline={timeline.latest} />
+            {/* <RouteStatistics class="p-5" route={route()} timeline={timeline.latest} /> */}
 
             <Suspense fallback={<div class="skeleton-loader min-h-48" />}>
               <RouteActions routeName={routeName()} route={route()} />
