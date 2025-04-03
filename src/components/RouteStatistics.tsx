ae2ecaa1fed4601316ffd6757ca89b7527907962
Shane Smiskol <shane@smiskol.com>
2025-04-03 00:10:02 -0700

    not needed

--- src/components/RouteStatistics.tsx
+++ src/components/RouteStatistics.tsx
@@ -6,7 +6,7 @@ import { formatDistance, formatRouteDuration } from '~/utils/format'
 import StatisticBar from './StatisticBar'
 
 const formatEngagement = (timeline: TimelineStatistics | undefined): string | undefined => {
-  if (!timeline || !timeline.duration) return undefined
+  if (!timeline) return undefined
   const { engagedDuration, duration } = timeline
   return `${(100 * (engagedDuration / duration)).toFixed(0)}%`
 }
