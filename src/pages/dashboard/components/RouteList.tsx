2cd6d34a8ba8ff86cb852899790908d3a2f3c73a
Cameron Clough <cameronjclough@gmail.com>
2025-04-04 01:38:25 +0100

    fix: show route distance and duration immediately

--- src/pages/dashboard/components/RouteList.tsx
+++ src/pages/dashboard/components/RouteList.tsx
@@ -49,7 +49,7 @@ const RouteCard: VoidComponent<RouteCardProps> = (props) => {
       />
 
       <CardContent>
-        <RouteStatistics route={timeline.loading ? undefined : props.route} timeline={timeline.latest} />
+        <RouteStatistics route={props.route} timeline={timeline.latest} />
       </CardContent>
     </Card>
   )
