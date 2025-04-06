3027c37c5d7e163ee2acf77f012167e1e2ccbcad
Trey Moen <trey@moen.ai>
2025-04-06 15:55:06 -0700

    less edge cases; just dont refetch on a new query mount

--- src/api/query-client.ts
+++ src/api/query-client.ts
@@ -2,11 +2,6 @@ import { QueryClient } from '@tanstack/solid-query'
 import { queries as uploadQueue } from '~/components/UploadQueue'
 import { queries as routes } from '~/pages/dashboard/activities/RouteActivity'
 
-const refreshAfterMs = (refreshAfterMs: number) => ({
-  staleTime: refreshAfterMs,
-  refetchOnMount: false,
-  refetchOnWindowFocus: false,
-})
 const pollingConfig = { retry: false, refetchInterval: 1000 }
 
 export const getAppQueryClient = () => {
@@ -14,7 +9,7 @@ export const getAppQueryClient = () => {
 
   queryClient.setQueryDefaults(uploadQueue.online(), pollingConfig)
   queryClient.setQueryDefaults(uploadQueue.offline(), pollingConfig)
-  queryClient.setQueryDefaults(routes.route(), refreshAfterMs(1000 * 60 * 60)) // 1 hour
+  queryClient.setQueryDefaults(routes.route(), { refetchOnMount: false, refetchOnWindowFocus: false })
 
   return queryClient
 }
