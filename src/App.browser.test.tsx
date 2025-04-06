c57ebcf248db7dec749b8957acf988c4ccc545b7
Trey Moen <trey@moen.ai>
2025-04-06 15:30:32 -0700

    fix QueryClientProvider not being set in tests

--- src/App.browser.test.tsx
+++ src/App.browser.test.tsx
@@ -4,10 +4,30 @@ import { configure, render, waitFor } from '@solidjs/testing-library'
 import { clearAccessToken, setAccessToken } from '~/api/auth/client'
 import * as Demo from '~/api/auth/demo'
 import { AppLayout, Routes } from './App'
+import { QueryClient, QueryClientProvider } from '@tanstack/solid-query'
+import { createMemoryHistory, MemoryRouter } from '@solidjs/router'
 
 const DEMO_LOG_ID = '000000dd--455f14369d'
 
-const renderApp = (location: string) => render(() => <Routes />, { location, wrapper: AppLayout })
+const createTestQueryClient = () => new QueryClient({ defaultOptions: { queries: { retry: false } } })
+
+const renderApp = (location: string) => {
+  // @solidjs/testing-library doesn't handle the QueryClientProvider wrapping, we need to own it here
+  const history = createMemoryHistory()
+  history.set({ value: location, scroll: false, replace: true })
+  return render(
+    () => (
+      <QueryClientProvider client={createTestQueryClient()}>
+        <MemoryRouter history={history}>
+          <Routes />
+        </MemoryRouter>
+      </QueryClientProvider>
+    ),
+    {
+      wrapper: AppLayout,
+    },
+  )
+}
 
 beforeAll(() => configure({ asyncUtilTimeout: 3000 }))
 beforeEach(() => clearAccessToken())
