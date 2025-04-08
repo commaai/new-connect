58abb7081c18e3414742d0f63345b558fd1c53cc
Cameron Clough <cameronjclough@gmail.com>
2025-04-08 16:06:20 +0100

    rename

--- src/components/RouteStatisticsBar.tsx
+++ src/components/RouteStatisticsBar.tsx
@@ -5,20 +5,22 @@ import type { Route } from '~/api/types'
 import { formatDistance, formatRouteDuration } from '~/utils/format'
 import StatisticBar from './StatisticBar'
 
-const formatEngagement = (timeline: RouteStatistics | undefined): string | undefined => {
-  if (!timeline || timeline.duration === 0) return undefined
-  const { engagedDuration, duration } = timeline
+const formatEngagement = (statistics: RouteStatistics | undefined): string | undefined => {
+  if (!statistics || statistics.duration === 0) return undefined
+  const { engagedDuration, duration } = statistics
   return `${(100 * (engagedDuration / duration)).toFixed(0)}%`
 }
 
-const RouteStatisticsBar: VoidComponent<{ class?: string; route: Route | undefined; timeline: RouteStatistics | undefined }> = (props) => {
+const RouteStatisticsBar: VoidComponent<{ class?: string; route: Route | undefined; statistics: RouteStatistics | undefined }> = (
+  props,
+) => {
   return (
     <StatisticBar
       class={props.class}
       statistics={[
         { label: 'Distance', value: () => formatDistance(props.route?.length) },
         { label: 'Duration', value: () => (props.route ? formatRouteDuration(props.route) : undefined) },
-        { label: 'Engaged', value: () => formatEngagement(props.timeline) },
+        { label: 'Engaged', value: () => formatEngagement(props.statistics) },
       ]}
     />
   )
