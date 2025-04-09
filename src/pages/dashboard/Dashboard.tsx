a5d4a4fafd75010e97fdd52ac8a66b5e99566429
Shane Smiskol <shane@smiskol.com>
2025-04-08 21:55:34 -0700

    Revert "fix: redirect user if they arent logged in" (#537)
    Reverts commaai/connect#512

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -3,7 +3,6 @@ import type { Component, JSXElement, VoidComponent } from 'solid-js'
 import { Navigate, type RouteSectionProps, useLocation } from '@solidjs/router'
 import clsx from 'clsx'
 
-import { isSignedIn } from '~/api/auth/client'
 import { USERADMIN_URL } from '~/api/config'
 import { getDevices } from '~/api/devices'
 import { getProfile } from '~/api/profile'
@@ -52,14 +51,25 @@ const DashboardDrawer: VoidComponent<{ devices: Device[] }> = (props) => {
           <Suspense fallback={<div class="min-h-16 rounded-md skeleton-loader" />}>
             <div class="flex max-w-full items-center px-3 rounded-md outline outline-1 outline-outline-variant min-h-16">
               <div class="shrink-0 size-10 inline-flex items-center justify-center rounded-full bg-primary-container text-on-primary-container">
-                <Icon name="person" filled />
+                <Icon name={!profile.loading && !profile.latest ? 'person_off' : 'person'} filled />
               </div>
-              <div class="min-w-0 mx-3">
-                <div class="truncate text-body-md text-on-surface">{profile()?.email}</div>
-                <div class="truncate text-label-sm text-on-surface-variant">{profile()?.user_id}</div>
-              </div>
-              <div class="grow" />
-              <IconButton name="logout" href="/logout" />
+              <Show
+                when={profile()}
+                fallback={
+                  <>
+                    <div class="mx-3">Not signed in</div>
+                    <div class="grow" />
+                    <IconButton name="login" href="/login" />
+                  </>
+                }
+              >
+                <div class="min-w-0 mx-3">
+                  <div class="truncate text-body-md text-on-surface">{profile()?.email}</div>
+                  <div class="truncate text-label-sm text-on-surface-variant">{profile()?.user_id}</div>
+                </div>
+                <div class="grow" />
+                <IconButton name="logout" href="/logout" />
+              </Show>
             </div>
           </Suspense>
         </ButtonBase>
@@ -119,9 +129,6 @@ const Dashboard: Component<RouteSectionProps> = () => {
   return (
     <Drawer drawer={<DashboardDrawer devices={devices()} />}>
       <Switch fallback={<TopAppBar leading={<DrawerToggleButton />}>No device</TopAppBar>}>
-        <Match when={!isSignedIn() || (!profile.loading && !profile.latest)}>
-          <Navigate href="/login" />
-        </Match>
         <Match when={urlState().dongleId === 'pair' || !!location.query.pair}>
           <PairActivity onPaired={refetch} />
         </Match>
@@ -151,6 +158,9 @@ const Dashboard: Component<RouteSectionProps> = () => {
               paneTwoContent={!!urlState().dateStr}
             />
           )}
+        </Match>
+        <Match when={!profile.loading && !profile.latest}>
+          <Navigate href="/login" />
         </Match>
         <Match when={getDefaultDongleId()} keyed>
           {(defaultDongleId) => <Navigate href={`/${defaultDongleId}`} />}
