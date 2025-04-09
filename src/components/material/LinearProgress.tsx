6129c9780cabdda4694841f3ac22cbe3b7c5ceb5
Cameron Clough <cameronjclough@gmail.com>
2025-04-09 21:06:26 +0100

    try this

--- src/components/material/LinearProgress.tsx
+++ src/components/material/LinearProgress.tsx
@@ -16,6 +16,10 @@ const colorClasses = {
 
 const LinearProgress: VoidComponent<LinearProgressProps> = (props) => {
   const color = () => colorClasses[props.color || 'primary']
+  const state = () => {
+    if (props.progress === undefined) return { indeterminate: true }
+    return { indeterminate: false, progress: props.progress }
+  }
   return (
     <div
       class={clsx(
@@ -25,21 +29,19 @@ const LinearProgress: VoidComponent<LinearProgressProps> = (props) => {
       )}
     >
       <Show
-        when={props.progress !== undefined}
+        when={state().indeterminate}
         fallback={
-          <>
-            <div
-              class={clsx('absolute inset-y-0 left-0 h-1 w-auto origin-left transition-indeterminate animate-indeterminate1', color().bar)}
-            />
-            <div
-              class={clsx('absolute inset-y-0 left-0 h-1 w-auto origin-left transition-indeterminate animate-indeterminate2', color().bar)}
-            />
-          </>
+          <div
+            class={clsx('absolute inset-y-0 left-0 h-1 transition-[background-color,width] duration-200 ease-linear', color().bar)}
+            style={{ width: `${props.progress! * 100}%` }}
+          />
         }
       >
         <div
-          class={clsx('absolute inset-y-0 left-0 h-1 transition-[background-color,width] duration-200 ease-linear', color().bar)}
-          style={{ width: `${props.progress! * 100}%` }}
+          class={clsx('absolute inset-y-0 left-0 h-1 w-auto origin-left transition-indeterminate animate-indeterminate1', color().bar)}
+        />
+        <div
+          class={clsx('absolute inset-y-0 left-0 h-1 w-auto origin-left transition-indeterminate animate-indeterminate2', color().bar)}
         />
       </Show>
     </div>
