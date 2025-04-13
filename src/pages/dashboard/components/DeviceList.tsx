316512f4beac7fadd9ebb1899ff918681181ea14
Cameron Clough <cameronjclough@gmail.com>
2025-04-13 23:19:18 +0100

    revert

--- src/pages/dashboard/components/DeviceList.tsx
+++ src/pages/dashboard/components/DeviceList.tsx
@@ -26,7 +26,7 @@ const DeviceList: VoidComponent<DeviceListProps> = (props) => {
   return (
     <List variant="nav" class={props.class}>
       <Suspense fallback={<div class="h-14 skeleton-loader rounded-xl" />}>
-        <For each={props.devices} fallback={<span class="text-md mx-2 text-on-surface-variant">No devices found</span>}>
+        <For each={props.devices}>
           {(device) => (
             <ListItem
               variant="nav"
