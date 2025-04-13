44cb9586458c8d4aeaf923cdac627575c3cde574
Cameron Clough <cameronjclough@gmail.com>
2025-04-13 22:40:26 +0100

    semantic HTML

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -98,6 +98,7 @@ const NoDeviceActivity: Component = () => {
     <>
       <TopAppBar
         class="font-bold"
+        component="h1"
         leading={
           <Show when={!modal()} fallback={<DrawerToggleButton />}>
             <img alt="" src="/images/comma-white.png" class="h-8" />
@@ -106,24 +107,22 @@ const NoDeviceActivity: Component = () => {
       >
         connect
       </TopAppBar>
-      <div class="flex flex-col gap-4 py-2 items-center mx-auto max-w-sm mt-4 sm:mt-8 md:mt-16">
+      <section class="flex flex-col gap-4 py-2 items-center mx-auto max-w-sm px-4 mt-4 sm:mt-8 md:mt-16">
         <h2 class="text-xl">Pair your device</h2>
         <p class="text-lg">Scan the QR code on your device</p>
-        <div class="flex flex-col gap-4 py-2">
-          <p class="text-md">If you cannot see a QR code, check the following:</p>
-          <ul class="text-md list-disc list-inside">
-            <li>Your device is connected to the internet</li>
-            <li>You have installed the latest version of openpilot</li>
-          </ul>
-          <p class="text-md">
-            If you still cannot see a QR code, your device may already be paired to another account. Make sure you have signed in to connect
-            with the same account you may have used previously.
-          </p>
-        </div>
-        <Button leading={<Icon name="add" />} href="/pair">
+        <p class="text-md mt-4">If you cannot see a QR code, check the following:</p>
+        <ul class="text-md list-disc list-inside">
+          <li>Your device is connected to the internet</li>
+          <li>You have installed the latest version of openpilot</li>
+        </ul>
+        <p class="text-md">
+          If you still cannot see a QR code, your device may already be paired to another account. Make sure you have signed in to connect
+          with the same account you may have used previously.
+        </p>
+        <Button class="mt-4" leading={<Icon name="add" />} href="/pair">
           Add new device
         </Button>
-      </div>
+      </section>
     </>
   )
 }
