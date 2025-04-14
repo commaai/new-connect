fdc5c57b6492524f8232a51b0a238b3e82daa57c
Cameron Clough <cameronjclough@gmail.com>
2025-04-14 23:06:59 +0100

    do this later

--- src/pages/dashboard/data.ts
+++ src/pages/dashboard/data.ts
@@ -6,24 +6,17 @@ import { getProfile } from '~/api/profile'
 import { getDeviceName } from '~/utils/device'
 import { resolved } from '~/utils/reactivity'
 
-export const [profile, { refetch: refetchProfile }] = createResource(accessToken, getProfile)
+export const [profile] = createResource(accessToken, getProfile)
 
 export const [devices, { refetch: refetchDevices }] = createResource(accessToken, getDevices)
 
 export const [currentDongleId, setCurrentDongleId] = createSignal<string>()
 
-const [currentDevice, { refetch: _refetchCurrentDevice }] = createResource(currentDongleId, (dongleId) => {
+export const [currentDevice] = createResource(currentDongleId, (dongleId) => {
   const device = resolved(devices) ? devices().find((device) => device.dongle_id === dongleId) : undefined
   if (device) return device
   return getDevice(dongleId)
 })
-const refetchCurrentDevice = () => {
-  const dongleId = currentDongleId()
-  if (!dongleId) return
-  if (resolved(devices) && devices().some((device) => device.dongle_id === dongleId)) return refetchDevices()
-  return _refetchCurrentDevice()
-}
-export { currentDevice, refetchCurrentDevice }
 
 export const currentDeviceName = createMemo(() => {
   const device = currentDevice()
