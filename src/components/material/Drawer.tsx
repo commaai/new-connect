514d62b409fbafac52a045ac0976c2711bfeb195
Trey Moen <trey@moen.ai>
2025-04-13 21:37:11 -0700

    remove round corner

--- src/components/material/Drawer.tsx
+++ src/components/material/Drawer.tsx
@@ -53,9 +53,7 @@ const Drawer: ParentComponent<DrawerProps> = (props) => {
           width: `${drawerWidth()}px`,
         }}
       >
-        <div class="flex size-full flex-col rounded-r-lg bg-surface-container text-on-surface-variant sm:rounded-r-none">
-          {props.drawer}
-        </div>
+        <div class="flex size-full flex-col bg-surface-container text-on-surface-variant sm:rounded-r-none">{props.drawer}</div>
       </nav>
 
       <main
