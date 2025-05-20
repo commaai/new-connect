3b5e9d5ae01d22539052a47cfe46a06f12fbfec9
Cameron Clough <cameronjclough@gmail.com>
2025-05-20 11:26:07 +0100

    comma threex -> 3X

--- src/utils/device.ts
+++ src/utils/device.ts
@@ -1,6 +1,21 @@
 import type { Device } from '~/api/types'
 
+function deviceTypePretty(device: Device) {
+  switch (device.device_type) {
+    case 'threex':
+      return 'comma 3X'
+    case 'neo':
+      return 'EON'
+    case 'freon':
+      return 'freon'
+    case 'unknown':
+      return 'unknown'
+    default:
+      return `comma ${device.device_type}`
+  }
+}
+
 export function getDeviceName(device: Device) {
   if (device.alias) return device.alias
-  return `comma ${device.device_type}`
+  return deviceTypePretty(device)
 }
